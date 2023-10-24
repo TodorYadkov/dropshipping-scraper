4af13d98d4d2321a7d2d261b6dd64ec549a3896b
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-24 12:57:47 +0300

    Add temporary cors headers

---
+++ server/middlewares/cors.js
@@ -0,0 +1,14 @@
+module.exports = () => (req, res, next) => {
+    // Here we can add which host can use our server
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With, Content-Type, X-Authorization'
+	);
+
+	next();
+};
