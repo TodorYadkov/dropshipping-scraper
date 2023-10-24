cd193ccd4a55d1b2add8d0d060e7e92093bc4e34
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-24 14:43:40 +0300

    update home controller

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,10 +1,6 @@
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
-	res.send('<h1>Hello World !</h1>');
-});
-
-homeController.get('/json', (req, res) => {
 	res.status(200).json({ hello: 'Hello World !' });
 });
 
