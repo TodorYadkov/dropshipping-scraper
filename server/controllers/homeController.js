498de85aa007945e1f6bec0cf8c30d774a959b1a
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-24 13:31:38 +0300

    Add json path for testing

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -4,4 +4,8 @@ homeController.get('/', (req, res) => {
 	res.send('<h1>Hello World !</h1>');
 });
 
+homeController.get('/json', (req, res) => {
+	res.status(200).json({ hello: 'Hello World !' });
+});
+
 module.exports = homeController;
