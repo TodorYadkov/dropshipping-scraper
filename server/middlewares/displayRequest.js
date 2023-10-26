3213612884b0074a93c5574cfb19a839754e4f5a
MaximKraychev <maxim.kraychev@gmail.com>
2023-10-26 17:30:24 +0300

    Add comment

--- server/middlewares/displayRequest.js
+++ server/middlewares/displayRequest.js
@@ -2,7 +2,7 @@ module.exports = () => (req, res, next) => {
     // Show each request
     console.log(`Request to: ${req.path} with method: ${req.method}`)
     // If there is information in the body - show it
-    const isEmpty = Object.keys(req.body).length === 0;
+    const isEmpty = Object.keys(req.body).length === 0;  // TODO remove when we have real users 
     if (isEmpty === false) {
         console.log(req.body);
     }
