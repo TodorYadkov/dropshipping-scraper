ecf4b25fa6ac7207ed0d048ff3c5eeb2410325a2
MaximKraychev <maxim.kraychev@gmail.com>
2023-10-26 15:15:52 +0300

    Add new routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,8 @@
 const homeController = require('../controllers/homeController');
+const productController = require('../controllers/productController');
 
 module.exports = (app) => {
 	app.use('/', homeController);
+	app.use('/products', productController);
+	app.all('*', (req, res) => res.status(404).json({ message: 'Not Found' }));
 };
