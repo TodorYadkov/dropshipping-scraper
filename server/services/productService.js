c735156614871ba8fd21a735dc63d30ac44e3c97
MaximKraychev <maxim.kraychev@gmail.com>
2023-10-26 14:46:09 +0300

    Add create product mongoose request

--- server/services/productService.js
+++ server/services/productService.js
@@ -5,3 +5,6 @@ const getSingleProduct = (productId) => Product.findById(productId);
 
 // GET ALL
 const getAllProduct = () => Product.find();
+
+// CREATE 
+const createProduct = (product) => Product.create(product);
