6edd8dc2909f739944a0f7e76de7a31534fe26aa
MaximKraychev <maxim.kraychev@gmail.com>
2023-10-27 15:06:16 +0300

    Add users endpoint

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,13 @@
 import { homeController } from '../controllers/homeController.js';
 import { productController } from '../controllers/productController.js';
+import { userController } from '../controllers/userController.js';
 import logRequests from '../middlewares/displayRequest.js';
 
 export default (app) => {
 	app.use(logRequests()); // Logging every request
 	app.use('/', homeController);
 	app.use('/products', productController);
+	app.use('/users', userController);
 	app.all('*', (req, res, next) => {
 		try {
 			throw new Error(`No content - path ${req.path} of method ${req.method} not found`);
