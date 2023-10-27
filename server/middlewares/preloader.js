17f473f50f5df2b8074be00b7dfbc2613d005f10
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-27 09:58:30 +0300

    Add preloader

---
+++ server/middlewares/preloader.js
@@ -0,0 +1,20 @@
+const preload =
+	(api, id = 'productId') =>
+	async (req, res, next) => {
+		try {
+			const paramsId = req.params[id];
+			const currentProduct = await api(paramsId);
+			if (currentProduct) {
+				res.locals.preload = currentProduct;
+				next();
+			} else {
+				throw new Error(`Entered ID - ${id} is invalid`, 404);
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
+
+module.exports = {
+	preload
+};
