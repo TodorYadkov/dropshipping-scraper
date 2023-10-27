e96c81aa05ffc280294f86f31e31eb6c5be8c363
MaximKraychev <maxim.kraychev@gmail.com>
2023-10-27 16:08:46 +0300

    Add isUserLogin guard to product endpoint

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,13 @@
 import { homeController } from '../controllers/homeController.js';
 import { productController } from '../controllers/productController.js';
 import { userController } from '../controllers/userController.js';
+import { isUserLogged } from '../middlewares/guards.js';
 import logRequests from '../middlewares/displayRequest.js';
 
 export default (app) => {
 	app.use(logRequests()); // Logging every request
 	app.use('/', homeController);
-	app.use('/products', productController);
+	app.use('/products', isUserLogged, productController);
 	app.use('/users', userController);
 	app.all('*', (req, res, next) => {
 		try {
