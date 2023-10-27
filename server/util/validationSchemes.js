67a3bc9aed7b173eeb917b10ef2143168ac207c2
Todor Yadkov <toni_12@abv.bg>
2023-10-27 20:24:53 +0300

    Add new property to save in DB email in lowercase

--- server/util/validationSchemes.js
+++ server/util/validationSchemes.js
@@ -16,7 +16,7 @@ const validateProductSchema = joi.object({
 const validateRegisterSchema = joi.object({
 	name: joi.string().required().trim().max(50),
 
-	email: joi.string().required().trim().email(),
+	email: joi.string().required().trim().email().lowercase(),
 
 	password: joi.string().required().trim().min(8).max(20),
 
@@ -29,9 +29,13 @@ const validateRegisterSchema = joi.object({
 });
 
 const validateLoginSchema = joi.object({
-	email: joi.string().required().trim().email(),
+	email: joi.string().required().trim().email().lowercase(),
 
-	password: joi.string().required().trim().min(8).max(20).lowercase(1).uppercase(1),
+	password: joi.string().required().trim().min(8).max(20),
+	// This doesn't work - password: joi.string().required().trim().min(8).max(20).lowercase(1).uppercase(1),
+	// TODO: If we want to add more complex password validation without any other library, we can use this regex
+	// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+	// /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 });
 
 export { validateProductSchema, validateRegisterSchema, validateLoginSchema };
