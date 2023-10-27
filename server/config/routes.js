63b96e3da19750e50846c0d713c52fbbc7a6e55e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-27 10:03:31 +0300

    Add extension to imported files

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,17 +1,17 @@
-const homeController = require('../controllers/homeController');
-const productController = require('../controllers/productController');
-const logRequests = require('../middlewares/displayRequest');
+const homeController = require('../controllers/homeController.js');
+const productController = require('../controllers/productController.js');
+const logRequests = require('../middlewares/displayRequest.js');
 
 module.exports = (app) => {
-	app.use(logRequests()) // Logging every request 
+	app.use(logRequests()); // Logging every request
 	app.use('/', homeController);
 	app.use('/products', productController);
 	app.all('*', (req, res, next) => {
-        try {
-            throw new Error(`No content - path ${req.path} of method ${req.method} not found`);
-        } catch (error) {
-            error.statusCode = 404;
-            next(error);
-        }
-    });
+		try {
+			throw new Error(`No content - path ${req.path} of method ${req.method} not found`);
+		} catch (error) {
+			error.statusCode = 404;
+			next(error);
+		}
+	});
 };
