ea4e733873602654327054db8960ab6fd578e983
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-27 10:04:10 +0300

    Add user session middleware

---
+++ server/middlewares/userSession.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const { tokenBlackList } = require('../util/tokenBlackList.js');
+
+module.exports = () => (req, res, next) => {
+	const userToken = req.headers['x-authorization'];
+
+	if (userToken) {
+		try {
+			if (tokenBlackList.has(userToken)) {
+				throw new Error('The token has already been used. Please sign in again.');
+			}
+
+			const decodedToken = jwt.verify(
+				userToken,
+				process.env.JWT_SECRET,
+				(err, decodedToken) => {
+					if (err) {
+						throw new Error('The token is invalid. Please sign in again.');
+					}
+
+					return decodedToken;
+				}
+			);
+
+			req.user = decodedToken;
+			req.userToken = userToken;
+		} catch (error) {
+			// Add status code and invoke global error handler
+			error.statusCode = 401;
+			return next(error);
+		}
+	}
+
+	next();
+};
