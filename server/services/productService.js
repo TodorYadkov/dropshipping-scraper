663e1f70be2502cd770e4546762ffe5433a1513f
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-10-27 12:01:07 +0300

    Add TODOS

--- server/services/productService.js
+++ server/services/productService.js
@@ -5,9 +5,11 @@ const getSingleProduct = (productId) => Product.findById(productId);
 
 // GET ALL
 const getAllProducts = () => Product.find();
+// TODO Add userId as parameter
 
 // CREATE 
 const createProduct = (product) => Product.create(product);
+// TODO Add userId as parameter
 
 // UPDATE
 const updateProduct = (productId, product) => Product.findByIdAndUpdate(productId, product, { runValidators: true, new: true });
