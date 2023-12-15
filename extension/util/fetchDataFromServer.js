4b8fe51094ef65a51b95dc333779235948fa7ebb
Todor Yadkov <toni_12@abv.bg>
2023-12-15 11:17:11 +0200

    Remove second call to contentScript

--- extension/util/fetchDataFromServer.js
+++ extension/util/fetchDataFromServer.js
@@ -2,14 +2,11 @@ import { getLink } from "../services/dataService.js";
 
 export async function fetchDataFromServer() {
     try {
-        // Make a GET request to your RESTful API
         const serverData = await getLink();
-        chrome.tabs.create({ url: serverData.url }, (tab) => {
-            chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['contentScript.js'] })
-                .catch(err => console.log(err));
-        });
+        
+        chrome.tabs.create({ url: serverData.url });
+
     } catch (error) {
         console.error('Error fetching data from server:', error);
     };
-
 }
