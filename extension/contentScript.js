a7c8dfff0b54ab7c4bf85522e3f0ba315226415c
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-15 17:52:31 +0200

    add xPath way for selecting DOM elements as backup

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -5,3 +5,9 @@ try {
 } catch (err) {
     chrome.runtime.sendMessage({ message: 'contentError', contentError: err });
 }
+
+// const xpathExpression = "//*[@id='title']/span[@id='productTitle']";
+// const titleElement = document.evaluate(xpathExpression, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+// const title = titleElement.textContent;
+
+// This is the alternative way using Xpath to this const title = document.querySelector('h1#title span#productTitle').textContent
