a183bd03c7021858dc7bcf2e2758cc42aea730c4
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-18 15:48:54 +0200

    Add storageActions

---
+++ extension/util/storageActions.js
@@ -0,0 +1,21 @@
+import { multiBrowser } from "../constants/constants.js";
+
+// TODO Chose between local and session storage
+
+export const setData = (data) => {
+    return new Promise((resolve) => {
+        multiBrowser.storage.local.set(data, () => resolve());
+    });
+};
+
+export const getData = (keys) => {
+    return new Promise((resolve) => {
+        multiBrowser.storage.local.get(keys, (result) => resolve(result));
+    });
+};
+
+export const removeData = (keys) => {
+    return new Promise((resolve) => {
+        multiBrowser.storage.local.remove(keys, () => resolve());
+    });
+};
