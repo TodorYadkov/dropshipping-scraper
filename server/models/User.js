a820b2a2b6e52312bf256452f0fcbf7ae7657a10
Todor Yadkov <toni_12@abv.bg>
2023-12-21 01:45:28 +0200

    Add new property on the model

--- server/models/User.js
+++ server/models/User.js
@@ -2,20 +2,20 @@ import { Schema, model } from 'mongoose';
 import { USER_ROLES } from '../environments/userRoles.js';
 
 const userSchema = new Schema({
-	name: {
-		type: String,
-		required: [true, 'Name is required!']
-	},
-	email: {
-		type: String,
-		required: [true, 'Email is required!'],
+    name: {
+        type: String,
+        required: [true, 'Name is required!']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required!'],
         unique: true,
         match: [/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,20}$/, 'Email is not valid!']
-	},
-	password: {
-		type: String,
-		required: [true, 'Password is required!']
-	},
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!']
+    },
     role: {
         type: String,
         required: [true, 'Role is required!'],
@@ -24,8 +24,22 @@ const userSchema = new Schema({
             message: '{VALUE} is not supported!'
         },
         default: USER_ROLES.USER
-    }
-});
+    },
+    isExtension: {
+        type: Boolean,
+        default: false
+    },
+    extensionsName: [{
+        type: String,
+        validate: {
+            validator: (value) => value.length === new Set(value).size,
+            message: 'Extension names must be unique!',
+        },
+        minlength: [5, 'Extension name must be at least 5 characters!'],
+        maxlength: [100, 'Extension name must be less than 100 characters!']
+    }]
+
+}, { timestamps: true });
 
 userSchema.index({ email: 1 }, {
     collation: {
