2d161bf28f00ca092f6d19c601bff0e39590cc07
Todor Yadkov <toni_12@abv.bg>
2023-12-21 18:55:48 +0200

    Create new model token black list

---
+++ server/models/TokenBlacklist.js
@@ -0,0 +1,25 @@
+import { Schema, Types, model } from 'mongoose';
+
+const tokenBlackListSchema = new Schema({
+    accessToken: {
+        type: String,
+        required: [true, 'Access token is required!']
+    },
+    extensionName: {
+        type: String,
+        default: 'React Client'
+    },
+    userId: {
+        type: Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User reference is required!']
+    }
+
+
+}, { timestamps: true });
+
+tokenBlackListSchema.index({ accessToken: 1 });
+
+const TokenBlackListSchema = model('TokenBlackList', tokenBlackListSchema);
+
+export { TokenBlackListSchema };
