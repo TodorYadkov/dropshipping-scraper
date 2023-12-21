2cdc78d0a0a5baaf72f2a70d7e8aa3f6572eca29
Todor Yadkov <toni_12@abv.bg>
2023-12-21 18:57:17 +0200

    Create black list token util

---
+++ server/util/getBlackListTokens.js
@@ -0,0 +1,19 @@
+import { getTokensBlackList } from "../services/tokenBlackListService.js";
+
+export async function getBlackListTokens(blackListState) {
+    try {
+        const rawTokens = await getTokensBlackList();
+
+        rawTokens.forEach(t => {
+            const accessToken = t.accessToken;
+            if (!blackListState.has(accessToken)) {
+                blackListState.add(accessToken);
+            }
+        })
+
+        return blackListState;
+
+    } catch (error) {
+        console.error('Error from token black list config: ', error);
+    }
+}
