57602765224e5cd3dd5279fb169ce0d50cbca025
Todor Yadkov <toni_12@abv.bg>
2023-12-21 18:56:07 +0200

    Add new token black list service

---
+++ server/services/tokenBlackListService.js
@@ -0,0 +1,10 @@
+import { TokenBlackListSchema } from "../models/TokenBlacklist.js";
+
+const getTokensBlackList = async () => TokenBlackListSchema.find({}, 'accessToken');
+
+const addTokenToBlackList = async (token) => TokenBlackListSchema.create(token);
+
+export {
+    getTokensBlackList,
+    addTokenToBlackList,
+}
