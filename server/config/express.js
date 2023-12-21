5c7eb97884f97a7a395a216e2b264a0942faac91
Todor Yadkov <toni_12@abv.bg>
2023-12-21 18:53:12 +0200

    Add new way to verified expired tokens

--- server/config/express.js
+++ server/config/express.js
@@ -2,11 +2,16 @@ import { json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from '../middlewares/cors.js';
 import userSession from '../middlewares/userSession.js';
+import tokenBlackListConfig from './tokenBlackListConfig.js';
+import tokenBlackListMiddleware from '../middlewares/tokenBlackListMiddleware.js';
 
-export default (app) => {
+export default async (app) => {
+	const tokenBlackList = await tokenBlackListConfig();
+
 	app.use(cors());
 	app.use(json());
 	app.use(urlencoded({ extended: true }));
+	app.use(tokenBlackListMiddleware(tokenBlackList));
 	app.use(userSession());
 	// app.use(cookieParser()); 				//	TODO.. add if needed
 };
