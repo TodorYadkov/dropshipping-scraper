4b87959e73bcd043f4ee76020fa674d4b3c27198
Todor Yadkov <toni_12@abv.bg>
2023-12-21 18:55:31 +0200

    Remove unnecessary check

--- server/middlewares/userSession.js
+++ server/middlewares/userSession.js
@@ -1,15 +1,10 @@
 import jwt from 'jsonwebtoken';
-import { tokenBlackList } from '../util/tokenBlackList.js';
 
 export default () => (req, res, next) => {
 	const userToken = req.headers['x-authorization'];
 
 	if (userToken) {
 		try {
-			if (tokenBlackList.has(userToken)) {
-				throw new Error('The token has already been used. Please sign in again.');
-			}
-
 			const decodedToken = jwt.verify(
 				userToken,
 				process.env.JWT_SECRET,
