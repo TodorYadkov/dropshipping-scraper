8c2acdc7a8b73b48c1273e18c048b8f9c0c3e02b
Todor Yadkov <toni_12@abv.bg>
2023-12-21 18:54:37 +0200

    Create token black list middleware

---
+++ server/middlewares/tokenBlackListMiddleware.js
@@ -0,0 +1,18 @@
+export default (tokenBlackList) => (req, res, next) => {
+    const userToken = req.headers['x-authorization'];
+
+    if (userToken) {
+        try {
+            if (tokenBlackList.has(userToken)) {
+                throw new Error('The token has already been used. Please sign in again.');
+            }
+
+        } catch (error) {
+            // Add status code and invoke global error handler
+            error.statusCode = 401;
+            return next(error);
+        }
+    }
+
+    next();
+};
