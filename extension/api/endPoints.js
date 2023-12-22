26af4abc4a010b46f32f5f52f1d012c49b6fe67b
Todor Yadkov <toni_12@abv.bg>
2023-12-22 21:18:39 +0200

    Add real end point

--- extension/api/endPoints.js
+++ extension/api/endPoints.js
@@ -1,8 +1,8 @@
 export const HOST = 'http://localhost:3000/';
 
 export const ENDPOINTS = {
-    TEST_GET: 'test',
-    TEST_POST: 'test',
+    GET_PRODUCT: 'products/extension/get-one',
+    UPDATE_PRODUCT: 'products/extension/put-one',
     LOGIN: 'users/login',
     LOGOUT: 'users/logout',
 }
