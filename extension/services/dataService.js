b57970c92e2e553b1895ce997f596eb27f76bf63
Todor Yadkov <toni_12@abv.bg>
2023-12-22 21:18:54 +0200

    Change request to real end point

--- extension/services/dataService.js
+++ extension/services/dataService.js
@@ -1,6 +1,6 @@
 import { api } from "../api/api.js";
 import { ENDPOINTS } from "../api/endPoints.js";
 
-export const getLink = () => api.get(ENDPOINTS.TEST_GET);
+export const getLink = () => api.get(ENDPOINTS.GET_PRODUCT);
 
-export const sendData = (data) => api.post(ENDPOINTS.TEST_POST, data);
+export const sendData = (data) => api.put(ENDPOINTS.UPDATE_PRODUCT, data);
