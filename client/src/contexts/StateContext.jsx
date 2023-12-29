87767d92de6f6c02be46d5b27e644b738a989b75
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-29 23:35:44 +0200

    Add StateContext

---
+++ client/src/contexts/StateContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ children }) => {
+
+    const [isOpenSideBar, setIsOpenSideBar] = useState(false);
+
+
+    function changeSideBarState(state) {
+        setIsOpenSideBar(state);
+    }
+
+    const values = {
+        isOpenSideBar,
+        changeSideBarState
+    };
+
+    return (
+        <StateContext.Provider value={values} >
+            {children}
+        </StateContext.Provider>
+    );
+}
