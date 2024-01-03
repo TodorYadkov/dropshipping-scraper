e06ac4a2e4f7a13713263372a68064f7aa920424
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-03 23:14:24 +0200

    Add useAuthContext hook

---
+++ client/src/hooks/useAuthContext.js
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+
+import { AuthContext } from "../contexts/AuthContext.jsx";
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+    return context;
+}
