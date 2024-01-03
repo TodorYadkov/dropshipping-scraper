89072b84fbe17a53f3e5765c5a4aba671e70dc59
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-03 23:18:34 +0200

    Add sidebar links

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -170,14 +170,24 @@ export const SideBar = () => {
                             <span className="mx-4">Modal</span>
                         </NavLink>
 
-                        <NavLink className={({ isActive }) => setActiveAndInactiveCss(isActive)} to="/blank" >
+                        <NavLink className={({ isActive }) => setActiveAndInactiveCss(isActive)} to="/register" >
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path
                                     d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"
                                 />
                             </svg>
 
-                            <span className="mx-4">Blank</span>
+                            <span className="mx-4">Register</span>
+                        </NavLink>
+
+                        <NavLink className={({ isActive }) => setActiveAndInactiveCss(isActive)} to="/login" >
+                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"
+                                />
+                            </svg>
+
+                            <span className="mx-4">Login</span>
                         </NavLink>
                     </nav>
                 </div>
