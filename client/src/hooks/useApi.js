ff720da82f398e6e93a5db2b5ee0628e5f854022
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-03 23:14:12 +0200

    Add useApi hook

---
+++ client/src/hooks/useApi.js
@@ -0,0 +1,11 @@
+import { api } from "../api/http-requester.js";
+import { useAuthContext } from "./useAuthContext.js";
+
+export const useApi = (service) => {
+    const { clearUserState, accessToken } = useAuthContext();
+    const decoratedApi = api(clearUserState, accessToken);
+
+    const { ...apiServices } = service(decoratedApi);
+    return apiServices;
+}
+
