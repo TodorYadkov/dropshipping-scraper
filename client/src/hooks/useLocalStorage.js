a7ce3c8f808d1f339f3e5af17de903e16c8f6dea
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-03 23:13:57 +0200

    Add useLocalStorage hook

---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import { TOKEN_NAME } from '../util/constants.js';
+
+export default function useLocalStorage() {
+    const [currentUserData, setCurrentUserData] = useState(() => {
+        try {
+            const localStorageData = JSON.parse(localStorage.getItem(TOKEN_NAME));
+            if (localStorageData) {
+                return localStorageData;
+            }
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+        return null;
+    });
+
+    function setUserState(userData) {
+        if (userData) {
+            localStorage.setItem(TOKEN_NAME, JSON.stringify(userData));
+            setCurrentUserData(userData);
+        }
+    }
+
+    function clearUserState() {
+        localStorage.removeItem(TOKEN_NAME);
+        setCurrentUserData(null);
+    }
+
+    return {
+        currentUserData,
+        setUserState,
+        clearUserState
+    };
+}
