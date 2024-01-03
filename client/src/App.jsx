953a65728046d27e97c14b4749e4535471b067ed
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-03 23:17:50 +0200

    Add ProductContext to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { api } from './util/http-requester.js';
+import { api } from './api/http-requester.js';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import { StateProvider } from './contexts/StateContext.jsx';
 
 import { Login } from './pages/Login.jsx';
 import { Register } from './pages/Register.jsx';
@@ -13,7 +15,6 @@ import { Card } from './components/Card.jsx';
 import { Modal } from './components/Modal.jsx';
 import { Header } from './components/Header.jsx';
 import { SideBar } from './components/SideBar.jsx';
-import { StateProvider } from './contexts/StateContext.jsx';
 import { Layout } from './components/Layout.jsx';
 import { SearchInput } from './components/SearchInput.jsx';
 import { AlertSuccess } from './components/Alerts/AlertSuccess.jsx';
@@ -26,31 +27,31 @@ import { ButtonDots } from './components/Buttons/ButtonDots.jsx';
 import { Pagination } from './components/Pagination.jsx';
 import { Form1 } from './components/Forms/Form1.jsx';
 import { Form2 } from './components/Forms/Form2.jsx';
+import { ProductProvider } from './contexts/ProductsContext.jsx';
 
 function App() {
-	const [heading, setHeading] = useState(null);
-
-	useEffect(() => {
-		api.get('/').then((data) => setHeading(data.hello));
-	}, []);
 
 	return (
-		<>
+		<AuthProvider>
 			<StateProvider>
-				<Layout>
-					<Routes>
-						<Route path='/' element={<Navigate to='/dashboard' />} />
-						<Route path='/dashboard' element={<Dashboard />} />
-						<Route path='/ui-elements' element={<AlertSuccess />} />
-						<Route path='/tables' element={<Table1 />} />
-						<Route path='/forms' element={<Form1 />} />
-						<Route path='/cards' element={<Card />} />
-						<Route path='/modal' element={<Modal />} />
-						<Route path='/blank' element={<h2>Blank Page</h2>} />
-					</Routes>
-				</Layout>
+				<ProductProvider>
+					<Layout>
+						<Routes>
+							<Route path='/' element={<Navigate to='/dashboard' />} />
+							<Route path='/dashboard' element={<Dashboard />} />
+							<Route path='/ui-elements' element={<AlertSuccess />} />
+							<Route path='/tables' element={<Table1 />} />
+							<Route path='/forms' element={<Form1 />} />
+							<Route path='/cards' element={<Card />} />
+							<Route path='/modal' element={<Modal />} />
+							<Route path='/blank' element={<h2>Blank Page</h2>} />
+							<Route path='/login' element={<Login />} />
+							<Route path='/register' element={<Register />} />
+						</Routes>
+					</Layout>
+				</ProductProvider>
 			</StateProvider>
-		</>
+		</AuthProvider>
 	);
 }
 
