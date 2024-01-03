1b4c4ee3d9fcfa127c479456b09dc4c7242f625c
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-03 23:11:57 +0200

    Add productService

---
+++ client/src/services/productService.js
@@ -0,0 +1,9 @@
+import { SERVER_PATHS } from "../util/paths.js";
+
+export const productService = (api) => {
+    const getProducts = async () => api.get(SERVER_PATHS.GET_ALL_PRODUCTS);
+
+    return {
+        getProducts
+    }
+}
