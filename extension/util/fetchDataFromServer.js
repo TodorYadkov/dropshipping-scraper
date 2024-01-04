98e858de3bd26deb3eb7cd0c483816ae7d264e1d
Todor Yadkov <toni_12@abv.bg>
2024-01-04 23:36:08 +0200

    Fix problem when no have products in DB

--- extension/util/fetchDataFromServer.js
+++ extension/util/fetchDataFromServer.js
@@ -1,17 +1,32 @@
 import { multiBrowser } from '../constants/constants.js';
 import { getLink } from '../services/dataService.js';
+import { getData, setData } from './storageActions.js';
 
 export async function fetchDataFromServer() {
 	try {
 		const productFromServer = await getLink();
 
+		if (!productFromServer['amazonUrl']) {
+			// Clear the alarm when the script is stopped
+			multiBrowser.alarms.clear('fetchDataAlarm');
+			await setData({ isScriptRunning: false, activeTabs: [] });
+
+			return {};
+		}
+
 		// Create a new tab
 		multiBrowser.tabs.create({
 			url: productFromServer.amazonUrl,
 			active: false
+		}, async (tab) => {
+			const { activeTabs } = await getData(['activeTabs']);
+			activeTabs.push(tab.id);
+
+			await setData({ activeTabs });
 		});
 
 		return productFromServer;
+
 	} catch (error) {
 		console.error('Error fetching data from server: ', error);
 	}
