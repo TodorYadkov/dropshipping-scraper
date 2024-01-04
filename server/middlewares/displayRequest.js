cf007531daf9ac987b9a4b4144ebaa122fe55f49
Todor Yadkov <toni_12@abv.bg>
2024-01-04 23:36:22 +0200

    Add new comment

--- server/middlewares/displayRequest.js
+++ server/middlewares/displayRequest.js
@@ -1,10 +1,11 @@
 export default () => (req, res, next) => {
     // Show each request
     console.log(`Request to: ${req.path} with method: ${req.method}`)
-    
+
     // If there is information in the body - show it
     const isEmpty = Object.keys(req.body).length === 0;  // TODO remove when we have real users 
     if (isEmpty === false) {
+        console.log('From: server/middlewares/displayRequest.js: TODO remove when we have real users');
         console.log(req.body);
     }
 
