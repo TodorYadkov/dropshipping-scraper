723dccd9dafe1a8e0abc0c7ccffe459f13ae48c1
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2024-01-06 00:51:55 +0200

    Change default export to named export

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { api } from './api/http-requester.js';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import { AppStateProvider } from './contexts/AppStateContext.jsx';
 
@@ -9,29 +7,17 @@ import { Login } from './pages/Auth/Login.jsx';
 import { Register } from './pages/Auth/Register.jsx';
 import { Dashboard } from './pages/Product/Dashboard.jsx';
 import { Table1 } from './components/Table1.jsx';
-import { Table2 } from './components/Table2.jsx';
-import { Table3 } from './components/Table3.jsx';
 import { Card } from './components/Card.jsx';
 import { Modal } from './components/Modal.jsx';
-import { Header } from './components/Header.jsx';
-import { SideBar } from './components/SideBar.jsx';
 import { Layout } from './components/Layout.jsx';
-import { SearchInput } from './components/SearchInput.jsx';
 import { AlertSuccess } from './components/Alerts/AlertSuccess.jsx';
-import { AlertInfo } from './components/Alerts/AlertInfo.jsx';
-import { AlertWarning } from './components/Alerts/AlertWarning.jsx';
-import { AlertError } from './components/Alerts/AlertError.jsx';
-import { ButtonPrimary } from './components/Buttons/ButtonPrimary.jsx';
-import { ButtonRefresh } from './components/Buttons/ButtonRefresh.jsx';
-import { ButtonDots } from './components/Buttons/ButtonDots.jsx';
-import { Pagination } from './components/Pagination.jsx';
 import { Form1 } from './components/Forms/Form1.jsx';
-import { Form2 } from './components/Forms/Form2.jsx';
 import { Logout } from './pages/Auth/Logout.jsx';
 import { RouteGuardPublic } from './guards/RouteGuardPublic.jsx';
 import { RoutGuardAuthenticated } from './guards/RouteGuardAuthenticated.jsx';
+import { CLIENT_PATHS } from './util/paths.js';
 
-function App() {
+export const App = () => {
 
 	return (
 		<AuthProvider>
@@ -41,21 +27,21 @@ function App() {
 
 						{/* Public routes */}
 						<Route element={<RouteGuardPublic />}>
-							<Route path='/login' element={<Login />} />
-							<Route path='/register' element={<Register />} />
+							<Route path={CLIENT_PATHS.LOGIN} element={<Login />} />
+							<Route path={CLIENT_PATHS.REGISTER} element={<Register />} />
 						</Route>
 
 						{/* Private routes */}
 						<Route element={<RoutGuardAuthenticated />}>
-							<Route path='/' element={<Navigate to='/dashboard' />} />
-							<Route path='/dashboard' element={<Dashboard />} />
+							<Route path='/' element={<Navigate to={CLIENT_PATHS.DASHBOARD} />} />
+							<Route path={CLIENT_PATHS.DASHBOARD} element={<Dashboard />} />
 							<Route path='/ui-elements' element={<AlertSuccess />} />
 							<Route path='/tables' element={<Table1 />} />
 							<Route path='/forms' element={<Form1 />} />
 							<Route path='/cards' element={<Card />} />
 							<Route path='/modal' element={<Modal />} />
 							<Route path='/blank' element={<h2>Blank Page</h2>} />
-							<Route path='/logout' element={<Logout />} />
+							<Route path={CLIENT_PATHS.LOGOUT} element={<Logout />} />
 						</Route>
 
 					</Routes>
@@ -64,5 +50,3 @@ function App() {
 		</AuthProvider>
 	);
 }
-
-export default App;
