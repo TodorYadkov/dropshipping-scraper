3e5f5683b2883102e5899ca4c5b5cbd7440b00ff
Todor Yadkov <toni_12@abv.bg>
2024-01-05 13:59:48 +0200

    Modified extension status

--- server/services/productService.js
+++ server/services/productService.js
@@ -1,30 +1,40 @@
 import { Product } from '../models/Product.js';
+import { ExtensionStatus } from '../models/ExtensionStatus.js';
 
 // Front-end requests
 // GET ALL
-const getAllProducts = (userId) => Product.find({ owner: userId });
+const getAllProducts = async (userId) => Product.find({ owner: userId });
 
 // GET ONE
-const getSingleProduct = (productId) => Product.findById(productId);
+const getSingleProduct = async (productId) => Product.findById(productId);
 
 // CREATE 
-const createProduct = (product, userId) => Product.create({ ...product, owner: userId });
+const createProduct = async (product, userId) => Product.create({ ...product, owner: userId });
 
 // UPDATE
-const updateProduct = (product, productId) => Product.findByIdAndUpdate(productId, product, { runValidators: true, new: true });
+const updateProduct = async (product, productId) => Product.findByIdAndUpdate(productId, product, { runValidators: true, new: true });
 
 // DELETE 
-const deleteProduct = (productId) => Product.findByIdAndDelete(productId, { returnDocument: true });
+const deleteProduct = async (productId) => Product.findByIdAndDelete(productId, { returnDocument: true });
 
 
 // Extension requests
 // GET ONE(LATEST) product for current user
-const getLatestUpdatedProduct = (userId) => Product.findOne({ owner: userId }).sort({ updatedAt: 1 });
+const getLatestUpdatedProduct = async (userId, extensionName) => {
+    const product = await Product.findOne({ owner: userId }).sort({ updatedAt: 1 });
+    const updatedExtensionStatus = await ExtensionStatus.findOneAndUpdate(
+        { userId, extensionName },
+        { isWork: true },
+        { new: true }
+    );
+
+    return product;
+};
 
 // UPDATE
-const updatedProductFromExtension = (product, productId) => Product.findByIdAndUpdate(productId, product, { runValidators: true, new: true });
+const updatedProductFromExtension = async (product, productId) => Product.findByIdAndUpdate(productId, product, { runValidators: true, new: true });
 
-const updatedProductOnError = (error, productId) => Product.findByIdAndUpdate(productId, { $set: { error } }, { runValidators: true, new: true });
+const updatedProductOnError = async (error, productId) => Product.findByIdAndUpdate(productId, { $set: { error } }, { runValidators: true, new: true });
 
 export {
     getSingleProduct,
