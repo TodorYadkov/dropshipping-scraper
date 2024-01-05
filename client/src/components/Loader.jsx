6041b8c687e8c8caf89bc3c48c114406dfe9de3c
Todor Yadkov <toni_12@abv.bg>
2024-01-05 22:51:31 +0200

    Center loader on each page

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,5 +2,9 @@ export const Loader = ({ width, height }) => {
     const w = width || 12;
     const h = height || 12;
 
-    return (<div className={`block mx-auto my-5 w-${w} h-${h} border-4 border-dashed rounded-full animate-spin-slow dark:border-indigo-600`}></div>);
+    return (
+        <div className="flex my-6 items-center justify-center">
+            <div className={`w-${w} h-${h} border-4 border-dashed rounded-full animate-spin-slow dark:border-indigo-600`}></div>
+        </div>
+    );
 }
