9e394a0b4620bc9f9f878f01d2ff06127e14c2bc
Todor Yadkov <toni_12@abv.bg>
2024-01-05 17:20:15 +0200

    Add new way to handle validation error

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,15 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useForm = (submitHandler, initialValues, validationFunction) => {
 	const [values, setValues] = useState(initialValues);
 	const [formErrorMessage, setFormErrorMessage] = useState({});
+	const [errorVisibility, setErrorVisibility] = useState(() => Object.fromEntries(Object.entries(initialValues).map(([k, v]) => [k, false])));
 	const [isInvalidForm, setIsInvalidForm] = useState(true);
+
+	useEffect(() => {
+		validateForm();
+
+	}, [values])
 
 	const onChange = (e) => {
 		setValues((state) => ({
 			...state,
 			[e.target.name]: e.target.value
 		}));
+
+		const currentErrors = validationFunction(e.target.name, e.target.value.trim());
+		setFormErrorMessage((state) => ({ ...state, ...currentErrors }));
 	};
 
 	const onSubmit = (e) => {
@@ -20,22 +29,22 @@ export const useForm = (submitHandler, initialValues, validationFunction) => {
 	};
 
 	const onBlur = (e) => {
-		const currentErrors = validationFunction(e.target.name, e.target.value.trim());
-		setFormErrorMessage((state) => ({ ...state, ...currentErrors }));
-		validateForm();
+		// const currentErrors = validationFunction(e.target.name, e.target.value.trim());
+		setErrorVisibility((state) => ({ ...state, [e.target.name]: true }));
 	}
 
-	function validateForm(){
+	function validateForm() {
 		setIsInvalidForm(
-			Object.values(values).some(value => value === '') || 
+			Object.values(values).some(value => value === '') ||
 			Object.values(formErrorMessage).some(error => error)
-			);
+		);
 	}
 
 	return {
 		values,
 		formErrorMessage,
 		isInvalidForm,
+		errorVisibility,
 		onChange,
 		onSubmit,
 		onBlur
