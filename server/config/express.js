94595e0eeb71d92a61c58d3035cde44ab60f4e37
Todor Yadkov <toni_12@abv.bg>
2024-01-05 13:58:32 +0200

    Add check for working extension

--- server/config/express.js
+++ server/config/express.js
@@ -3,6 +3,7 @@ import cors from '../middlewares/cors.js';
 import userSession from '../middlewares/userSession.js';
 import tokenBlackListConfig from './tokenBlackListConfig.js';
 import tokenBlackListMiddleware from '../middlewares/tokenBlackListMiddleware.js';
+import { checkWorkingExtension, stopCheckWorkingExtension } from '../util/checkWorkingExtension.js';
 
 export default async (app) => {
 	const tokenBlackList = await tokenBlackListConfig();
@@ -13,4 +14,13 @@ export default async (app) => {
 	app.use(tokenBlackListMiddleware(tokenBlackList));
 	app.use(userSession());
 	// app.use(cookieParser()); 				//	TODO.. add if needed
+
+	// Run the scheduled task
+	checkWorkingExtension();
+
+	// Handle shutdown to stop the checkWorkingExtension
+	process.on('SIGINT', () => {
+		stopCheckWorkingExtension();
+		process.exit();
+	});
 };
