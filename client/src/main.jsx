fcebf46184e29216b44ccf57710c332b7b08e9c9
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2024-01-06 00:50:49 +0200

    Change to named import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App.jsx';
+import { App } from './App.jsx';
 import './index.css';
 import { ErrorBoundary } from './guards/ErrorBoundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+	<StrictMode>
 		<BrowserRouter>
 			<ErrorBoundary>
 				<App />
 			</ErrorBoundary>
 		</BrowserRouter>
-	</React.StrictMode>
+	</StrictMode>
 );
