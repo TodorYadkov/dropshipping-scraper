8b1f577898f22f09f49007dd19d589e53b0e4c96
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-05 14:23:37 +0200

    Change AppStateContext to use useReducer

---
+++ client/src/contexts/AppStateContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from 'react';
+import { reducer } from './reducer.js';
+import { REDUCER_TYPES } from '../util/constants.js';
+
+const initialState = {
+	[REDUCER_TYPES.IS_SIDE_BAR_OPEN]: false,
+	[REDUCER_TYPES.PRODUCTS]: [],
+}
+
+export const AppStateContext = createContext();
+AppStateContext.displayName = 'AppStateContext';
+
+export const AppStateProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	function changeSideBarState(value) {
+		dispatch({ type: REDUCER_TYPES.IS_SIDE_BAR_OPEN, value });
+	}
+
+	function setProducts(value) {
+		dispatch({ type: REDUCER_TYPES.PRODUCTS, value });
+	}
+
+	const values = {
+		state,
+		changeSideBarState,
+		setProducts
+	};
+
+	return (
+		<AppStateContext.Provider value={values}>{children}</AppStateContext.Provider>
+	);
+};
