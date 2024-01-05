d8047ea5658c59f3f5ca522dc218b83bb65fabf2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2024-01-05 03:41:15 +0200

    Create useForm hook

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function useForm(submitHandler, initialValues, validationFunction) {
+	const [values, setValues] = useState(initialValues);
+	const [formErrorMessage, setFormErrorMessage] = useState({});
+	const [isInvalidForm, setIsInvalidForm] = useState(true);
+
+	const onChange = (e) => {
+		setValues((state) => ({
+			...state,
+			[e.target.name]: e.target.value
+		}));
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const trimmedValues = Object.fromEntries(Object.entries(values).map(([k, v]) => [k, typeof v === 'string' ? v.trim() : v]));
+		submitHandler(trimmedValues);
+	};
+
+	const onBlur = (e) => {
+		const currentErrors = validationFunction(e.target.name, e.target.value.trim());
+		setFormErrorMessage((state) => ({ ...state, ...currentErrors }));
+		validateForm();
+	}
+
+	function validateForm(){
+		setIsInvalidForm(
+			Object.values(values).some(value => value === '') || 
+			Object.values(formErrorMessage).some(error => error)
+			);
+	}
+
+	return {
+		values,
+		formErrorMessage,
+		isInvalidForm,
+		onChange,
+		onSubmit,
+		onBlur
+	};
+}
