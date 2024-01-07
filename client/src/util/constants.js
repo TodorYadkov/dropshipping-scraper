2c7aed20e05dfbf4b1b4b2d01191c4ca1490f0a5
Todor Yadkov <toni_12@abv.bg>
2024-01-07 15:13:04 +0200

    Add TABLE_BODY_TYPES constants

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -10,8 +10,13 @@ export const AUTH_FORM_KEYS = {
 // eslint-disable-next-line no-control-regex
 export const patternEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
 
-
 export const REDUCER_TYPES = {
 	IS_SIDE_BAR_OPEN: 'isSideBarOpen',
 	PRODUCTS: 'products'
 }
+
+export const TABLE_BODY_TYPES = {
+	PRODUCT: 'product',
+	EXTENSION: 'extension',
+	USER: 'user'
+};
