6f87525113553e0d038c4ed68c506140b5bb85f7
Todor Yadkov <toni_12@abv.bg>
2024-01-07 15:12:44 +0200

    Implement table component

--- client/src/pages/Product/Dashboard.jsx
+++ client/src/pages/Product/Dashboard.jsx
@@ -1,11 +1,16 @@
 import { useEffect } from 'react';
 import { useApi } from '../../hooks/useApi.js';
+
+import { REDUCER_TYPES, TABLE_BODY_TYPES } from '../../util/constants.js';
+
 import { productService } from '../../services/productService.js';
 import { useAppStateContext } from '../../hooks/useAppStateContext.js';
 
+import { Table } from '../../components/Tables/Table.jsx';
+
 export const Dashboard = () => {
 	const { getProducts } = useApi(productService);
-	const { setProducts } = useAppStateContext();
+	const { appState, setProducts } = useAppStateContext();
 
 	useEffect(() => {
 		(async function () {
@@ -139,84 +144,7 @@ export const Dashboard = () => {
 
 			<div className="mt-8" />
 
-			<div className="flex flex-col mt-8">
-				<div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-					<div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-						<table className="min-w-full">
-							<thead>
-								<tr>
-									<th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-										Name
-									</th>
-									<th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-										Title
-									</th>
-									<th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-										Status
-									</th>
-									<th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-										Role
-									</th>
-									<th className="px-6 py-3 border-b border-gray-200 bg-gray-50" />
-								</tr>
-							</thead>
-
-							<tbody className="bg-white">
-								<tr>
-									<td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-										<div className="flex items-center">
-											<div className="flex-shrink-0 w-10 h-10">
-												<img
-													className="w-10 h-10 rounded-full"
-													src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-													alt=""
-												/>
-											</div>
-
-											<div className="ml-4">
-												<div className="text-sm font-medium leading-5 text-gray-900">
-													{'Name'}
-												</div>
-												<div className="text-sm leading-5 text-gray-500">
-													{'Email'}
-												</div>
-											</div>
-										</div>
-									</td>
-
-									<td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-										<div className="text-sm leading-5 text-gray-900">
-											{'Title'}
-										</div>
-										<div className="text-sm leading-5 text-gray-500">
-											{'Title 2'}
-										</div>
-									</td>
-
-									<td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-										<span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
-											{'Status'}
-										</span>
-									</td>
-
-									<td className="px-6 py-4 text-sm leading-5 text-gray-500 border-b border-gray-200 whitespace-nowrap">
-										{'role'}
-									</td>
-
-									<td className="px-6 py-4 text-sm font-medium leading-5 text-right border-b border-gray-200 whitespace-nowrap">
-										<a
-											href="#"
-											className="text-indigo-600 hover:text-indigo-900"
-										>
-											Edit
-										</a>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
+			{<Table data={appState[REDUCER_TYPES.PRODUCTS]} type={TABLE_BODY_TYPES.PRODUCT} />}
 		</div>
 	);
 };
