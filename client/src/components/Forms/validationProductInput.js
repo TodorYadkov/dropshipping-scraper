9b28678638a3a03db40775b6da6a504f1734738d
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-08 21:30:41 +0200

    Add base validations for product form

---
+++ client/src/components/Forms/validationProductInput.js
@@ -0,0 +1,16 @@
+import { PRODUCT_FORM_KEYS } from "../../util/constants.js";
+
+export const validationProductInput = (inputName, inputValue) => {
+    switch (inputName) {
+        case PRODUCT_FORM_KEYS.AMAZON:
+            if (inputValue == '') {
+                return { [inputName]: 'Amazon Url is required' };
+            }
+        case PRODUCT_FORM_KEYS.EBAY:
+            if (inputValue == '') {
+                return { [inputName]: 'Ebay Url is required' };
+            }
+        default:
+            return { [inputName]: '' };
+    }
+}
