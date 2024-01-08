b57c566761ff9138fc2ff4c6d86195548de01ed8
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-08 16:52:28 +0200

    Removed top margin on Pagination component

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,8 +1,8 @@
 export const Pagination = () => {
 	return (
-		<div className="mt-4">
-			<div className="flex justify-center px-4 py-4 overflow-x-auto bg-white rounded-md">
-				<div className="flex mr-4 rounded">
+		<div >
+			<div className="flex justify-center px-4 py-4 overflow-x-auto bg-white rounded-b-lg">
+				<div className="flex mr-4 rounded-b-lg">
 					<a
 						href="#"
 						className="px-3 py-2 ml-0 leading-tight text-indigo-700 bg-white border border-r-0 border-gray-200 rounded-l hover:bg-indigo-500 hover:text-white"
