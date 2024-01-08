fa6369db3eb4bc85b61a3c3e284b48e34273a957
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-08 21:30:16 +0200

    Add title prop for Button component

--- client/src/components/Buttons/ButtonPrimary.jsx
+++ client/src/components/Buttons/ButtonPrimary.jsx
@@ -1,7 +1,7 @@
-export const ButtonPrimary = () => {
+export const ButtonPrimary = ({title, toggle}) => {
 	return (
-		<button className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
-			Primary
+		<button onClick={toggle} className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
+			{title}
 		</button>
 	);
 };
