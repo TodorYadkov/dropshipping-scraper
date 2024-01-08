0748d63aeb1568b23290ff920fc2dc1169e56f7d
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-08 16:54:52 +0200

    Add tailwind class

--- client/src/components/Tables/TableHeader.jsx
+++ client/src/components/Tables/TableHeader.jsx
@@ -4,7 +4,7 @@ export const TableHeader = ({ headings }) => {
         <thead>
             <tr>
                 {headings.map((heading, index) => (
-                    <th key={index} className="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                    <th key={index} className="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200 whitespace-nowrap">
                         {heading}
                     </th>
                 ))}
