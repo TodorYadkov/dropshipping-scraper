99c7e1761b851cfe9b2c0e52f45754edf5374540
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-08 21:12:49 +0200

    Add keys for product form

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -7,12 +7,18 @@ export const AUTH_FORM_KEYS = {
 	extensionName: 'extensionName'
 };
 
+export const PRODUCT_FORM_KEYS = {
+	AMAZON: 'amazonUrl',
+	EBAY: 'ebayUrl'
+}
+
 // eslint-disable-next-line no-control-regex
 export const patternEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
 
 export const REDUCER_TYPES = {
 	IS_SIDE_BAR_OPEN: 'isSideBarOpen',
-	PRODUCTS: 'products'
+	PRODUCTS: 'products',
+	ADD_PRODUCT: 'addProduct',
 }
 
 export const TABLE_BODY_TYPES = {
