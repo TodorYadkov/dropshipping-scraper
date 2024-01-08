aba2ed6b1a1631f1b5cfc7aacfb64d52a291552c
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-08 21:27:58 +0200

    Add createProduct service

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -3,7 +3,10 @@ import { SERVER_PATHS } from '../util/paths.js';
 export const productService = (api) => {
 	const getProducts = async () => api.get(SERVER_PATHS.GET_ALL_PRODUCTS);
 
+	const createProduct = async () => api.post(SERVER_PATHS.POST_PRODUCT);
+
 	return {
-		getProducts
+		getProducts,
+		createProduct
 	};
 };
