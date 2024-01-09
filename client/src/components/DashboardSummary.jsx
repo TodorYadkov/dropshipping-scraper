e069cc2113ff0560f0d9b11dc8c81f514a9151e7
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:09:34 +0200

    Add summary properties

--- client/src/components/DashboardSummary.jsx
+++ client/src/components/DashboardSummary.jsx
@@ -1,89 +1,98 @@
-export const DashboardSummary = () => {
+export const DashboardSummary = ({
+    extensionsCount,
+    extensionsIsWork,
+    extensionsIsLogin,
+    extensionsNotWorked,
+    productsCount,
+    productsErrorCount,
+}) => {
+
     return (
-        <div className="mt-4">
-            <div className="flex flex-wrap -mx-6">
-                <div className="w-full px-6 sm:w-1/2 xl:w-1/3">
-                    <div className="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+        <div className="mt-4 cursor-default">
+            <div className="flex items-center flex-wrap -mx-6">
+                <div className="w-full px-6 sm:w-1/2 xl:w-1/3 text-center sm:text-left">
+                    <div className="flex flex-col sm:flex-row items-center justify-center px-5 py-6 bg-white rounded-md shadow-sm">
                         <div className="p-3 bg-indigo-600 bg-opacity-75 rounded-full">
                             <svg
                                 className="w-8 h-8 text-white"
-                                viewBox="0 0 28 30"
-                                fill="none"
+                                viewBox="-40 0 550 550"
+                                fill="white"
                                 xmlns="http://www.w3.org/2000/svg"
                             >
-                                <path
-                                    d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z"
-                                    fill="currentColor"
-                                />
+                                <path d="M192 104.8c0-9.2-5.8-17.3-13.2-22.8C167.2 73.3 160 61.3 160 48c0-26.5 28.7-48 64-48s64 21.5 64 48c0 13.3-7.2 25.3-18.8 34c-7.4 5.5-13.2 13.6-13.2 22.8c0 12.8 10.4 23.2 23.2 23.2H336c26.5 0 48 21.5 48 48v56.8c0 12.8 10.4 23.2 23.2 23.2c9.2 0 17.3-5.8 22.8-13.2c8.7-11.6 20.7-18.8 34-18.8c26.5 0 48 28.7 48 64s-21.5 64-48 64c-13.3 0-25.3-7.2-34-18.8c-5.5-7.4-13.6-13.2-22.8-13.2c-12.8 0-23.2 10.4-23.2 23.2V464c0 26.5-21.5 48-48 48H279.2c-12.8 0-23.2-10.4-23.2-23.2c0-9.2 5.8-17.3 13.2-22.8c11.6-8.7 18.8-20.7 18.8-34c0-26.5-28.7-48-64-48s-64 21.5-64 48c0 13.3 7.2 25.3 18.8 34c7.4 5.5 13.2 13.6 13.2 22.8c0 12.8-10.4 23.2-23.2 23.2H48c-26.5 0-48-21.5-48-48V343.2C0 330.4 10.4 320 23.2 320c9.2 0 17.3 5.8 22.8 13.2C54.7 344.8 66.7 352 80 352c26.5 0 48-28.7 48-64s-21.5-64-48-64c-13.3 0-25.3 7.2-34 18.8C40.5 250.2 32.4 256 23.2 256C10.4 256 0 245.6 0 232.8V176c0-26.5 21.5-48 48-48H168.8c12.8 0 23.2-10.4 23.2-23.2z" />
                             </svg>
                         </div>
 
                         <div className="mx-5">
                             <h4 className="text-2xl font-semibold text-gray-700">
-                                8,282
+                                {extensionsCount}
                             </h4>
-                            <div className="text-gray-500">New Users</div>
+                            <p className="text-gray-500">Extensions</p>
                         </div>
-                    </div>
-                </div>
 
-                <div className="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
-                    <div className="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
-                        <div className="p-3 bg-blue-600 bg-opacity-75 rounded-full">
+                        <div className="p-3 bg-yellow-600 bg-opacity-75 rounded-full">
                             <svg
                                 className="w-8 h-8 text-white"
-                                viewBox="0 0 28 28"
-                                fill="none"
+                                viewBox="0 0 576 512"
+                                fill="white"
                                 xmlns="http://www.w3.org/2000/svg"
                             >
-                                <path
-                                    d="M4.19999 1.4C3.4268 1.4 2.79999 2.02681 2.79999 2.8C2.79999 3.57319 3.4268 4.2 4.19999 4.2H5.9069L6.33468 5.91114C6.33917 5.93092 6.34409 5.95055 6.34941 5.97001L8.24953 13.5705L6.99992 14.8201C5.23602 16.584 6.48528 19.6 8.97981 19.6H21C21.7731 19.6 22.4 18.9732 22.4 18.2C22.4 17.4268 21.7731 16.8 21 16.8H8.97983L10.3798 15.4H19.6C20.1303 15.4 20.615 15.1004 20.8521 14.6261L25.0521 6.22609C25.2691 5.79212 25.246 5.27673 24.991 4.86398C24.7357 4.45123 24.2852 4.2 23.8 4.2H8.79308L8.35818 2.46044C8.20238 1.83722 7.64241 1.4 6.99999 1.4H4.19999Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M22.4 23.1C22.4 24.2598 21.4598 25.2 20.3 25.2C19.1403 25.2 18.2 24.2598 18.2 23.1C18.2 21.9402 19.1403 21 20.3 21C21.4598 21 22.4 21.9402 22.4 23.1Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M9.1 25.2C10.2598 25.2 11.2 24.2598 11.2 23.1C11.2 21.9402 10.2598 21 9.1 21C7.9402 21 7 21.9402 7 23.1C7 24.2598 7.9402 25.2 9.1 25.2Z"
-                                    fill="currentColor"
-                                />
+                                <path d="M96 0C78.3 0 64 14.3 64 32v96h64V32c0-17.7-14.3-32-32-32zM288 0c-17.7 0-32 14.3-32 32v96h64V32c0-17.7-14.3-32-32-32zM32 160c-17.7 0-32 14.3-32 32s14.3 32 32 32v32c0 77.4 55 142 128 156.8V480c0 17.7 14.3 32 32 32s32-14.3 32-32V412.8c12.3-2.5 24.1-6.4 35.1-11.5c-2.1-10.8-3.1-21.9-3.1-33.3c0-80.3 53.8-148 127.3-169.2c.5-2.2 .7-4.5 .7-6.8c0-17.7-14.3-32-32-32H32zM432 512a144 144 0 1 0 0-288 144 144 0 1 0 0 288zm47.9-225c4.3 3.7 5.4 9.9 2.6 14.9L452.4 356H488c5.2 0 9.8 3.3 11.4 8.2s-.1 10.3-4.2 13.4l-96 72c-4.5 3.4-10.8 3.2-15.1-.6s-5.4-9.9-2.6-14.9L411.6 380H376c-5.2 0-9.8-3.3-11.4-8.2s.1-10.3 4.2-13.4l96-72c4.5-3.4 10.8-3.2 15.1 .6z" />
                             </svg>
                         </div>
 
                         <div className="mx-5">
                             <h4 className="text-2xl font-semibold text-gray-700">
-                                200,521
+                                {extensionsIsLogin}
                             </h4>
-                            <div className="text-gray-500">
-                                Total Orders
-                            </div>
+                            <p className="text-gray-500">Logged</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
-                    <div className="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+                <div className="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 sm:mt-0 text-center sm:text-left">
+                    <div className="flex flex-col sm:flex-row items-center justify-center px-5 py-6 bg-white rounded-md shadow-sm">
+                        <div className="p-3 bg-green-600 bg-opacity-75 rounded-full">
+                            <svg
+                                className="w-8 h-8 text-white"
+                                viewBox="0 0 576 512"
+                                fill="white"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M96 0C78.3 0 64 14.3 64 32v96h64V32c0-17.7-14.3-32-32-32zM288 0c-17.7 0-32 14.3-32 32v96h64V32c0-17.7-14.3-32-32-32zM32 160c-17.7 0-32 14.3-32 32s14.3 32 32 32v32c0 77.4 55 142 128 156.8V480c0 17.7 14.3 32 32 32s32-14.3 32-32V412.8c12.3-2.5 24.1-6.4 35.1-11.5c-2.1-10.8-3.1-21.9-3.1-33.3c0-80.3 53.8-148 127.3-169.2c.5-2.2 .7-4.5 .7-6.8c0-17.7-14.3-32-32-32H32zM576 368a144 144 0 1 0 -288 0 144 144 0 1 0 288 0zm-76.7-43.3c6.2 6.2 6.2 16.4 0 22.6l-72 72c-6.2 6.2-16.4 6.2-22.6 0l-40-40c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L416 385.4l60.7-60.7c6.2-6.2 16.4-6.2 22.6 0z" />
+                            </svg>
+                        </div>
+
+                        <div className="mx-5">
+                            <h4 className="text-2xl font-semibold text-gray-700">
+                                {extensionsIsWork}
+                            </h4>
+                            <p className="text-gray-500">Working</p>
+                        </div>
+
+                        <div className="p-3 bg-red-600 bg-opacity-75 rounded-full">
+                            <svg
+                                className="w-8 h-8 text-white"
+                                viewBox="0 0 576 512"
+                                fill="white"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M96 0C78.3 0 64 14.3 64 32v96h64V32c0-17.7-14.3-32-32-32zM288 0c-17.7 0-32 14.3-32 32v96h64V32c0-17.7-14.3-32-32-32zM32 160c-17.7 0-32 14.3-32 32s14.3 32 32 32v32c0 77.4 55 142 128 156.8V480c0 17.7 14.3 32 32 32s32-14.3 32-32V412.8c12.3-2.5 24.1-6.4 35.1-11.5c-2.1-10.8-3.1-21.9-3.1-33.3c0-80.3 53.8-148 127.3-169.2c.5-2.2 .7-4.5 .7-6.8c0-17.7-14.3-32-32-32H32zM432 512a144 144 0 1 0 0-288 144 144 0 1 0 0 288zm59.3-180.7L454.6 368l36.7 36.7c6.2 6.2 6.2 16.4 0 22.6s-16.4 6.2-22.6 0L432 390.6l-36.7 36.7c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6L409.4 368l-36.7-36.7c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L432 345.4l36.7-36.7c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z" />
+                            </svg>
+                        </div>
+
+                        <div className="mx-5">
+                            <h4 className="text-2xl font-semibold text-gray-700">
+                                {extensionsNotWorked}
+                            </h4>
+                            <p className="text-gray-500">Not Working</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0 text-center sm:text-left">
+                    <div className="flex flex-col sm:flex-row items-center justify-center px-5 py-6 bg-white rounded-md shadow-sm">
                         <div className="p-3 bg-pink-600 bg-opacity-75 rounded-full">
                             <svg
                                 className="w-8 h-8 text-white"
@@ -108,11 +117,25 @@ export const DashboardSummary = () => {
 
                         <div className="mx-5">
                             <h4 className="text-2xl font-semibold text-gray-700">
-                                215,542
+                                {productsCount}
                             </h4>
-                            <div className="text-gray-500">
-                                Available Products
-                            </div>
+                            <p className="text-gray-500">Products</p>
+                        </div>
+
+                        <div className="p-3 bg-red-600 bg-opacity-75 rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                className="w-8 h-8 text-white"
+                                fill="white"
+                                viewBox="0 30 512 512">
+                                <path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
+                            </svg>
+                        </div>
+
+                        <div className="mx-5">
+                            <h4 className="text-2xl font-semibold text-gray-700">
+                                {productsErrorCount}
+                            </h4>
+                            <p className="text-gray-500">Errors</p>
                         </div>
                     </div>
                 </div>
