eb7e4b6852adf00d305375656abb20ec48324fef
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:10:23 +0200

    Add optional css property to use custom margin

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,9 @@
-export const Loader = ({ width, height }) => {
+export const Loader = ({ width, height, margin }) => {
     const w = width || 12;
     const h = height || 12;
 
     return (
-        <div className="flex my-6 items-center justify-center">
+        <div className={`flex items-center justify-center ${margin ? margin : 'my-6'}`}>
             <div className={`w-${w} h-${h} border-4 border-dashed rounded-full animate-spin-slow dark:border-indigo-600`}></div>
         </div>
     );
