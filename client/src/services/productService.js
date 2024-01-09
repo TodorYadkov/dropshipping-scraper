28b0033461ab2876c0c5b89c9ca3d058b0dfca1d
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:19:16 +0200

    Refactoring

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -3,7 +3,7 @@ import { SERVER_PATHS } from '../util/paths.js';
 export const productService = (api) => {
 	const getProducts = async () => api.get(SERVER_PATHS.GET_ALL_PRODUCTS);
 
-	const createProduct = async () => api.post(SERVER_PATHS.POST_PRODUCT);
+	const createProduct = async (data) => api.post(SERVER_PATHS.CREATE_PRODUCT, data);
 
 	return {
 		getProducts,
