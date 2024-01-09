05623764ec2f06106629782bcd070e7361b07604
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:13:22 +0200

    Add optional close on backdrop

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,14 +1,13 @@
 import { ModalHeader } from "./ModalHeader.jsx";
 
-export const Modal = ({ title, Content, toggleModal }) => {
+export const Modal = ({ title, Content, toggleModal, backdrop = true }) => {
 
 	return (
 		<div className={`z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center`} >
+			<div className="absolute w-full h-full bg-gray-900 opacity-50 modal-overlay" onClick={backdrop ? toggleModal : null} />
 
-			<div className="absolute w-full h-full bg-gray-900 opacity-50 modal-overlay" onClick={toggleModal} />
-
-			<div className="z-50 w-full mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-md">
-				< div className="text-left modal-content" >
+			<div className="z-50 w-full mx-auto overflow-y-auto bg-gray-200 rounded shadow-lg modal-container md:max-w-md">
+				<div className="text-left modal-content">
 					{/* <!-- Header --> */}
 					<ModalHeader title={title} toggleModal={toggleModal} />
 
