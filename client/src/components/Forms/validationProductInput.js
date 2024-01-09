bd4b93e0ab38f33ef616c18ad6aeb5f01d00323d
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:12:31 +0200

    Add additional equal sign

--- client/src/components/Forms/validationProductInput.js
+++ client/src/components/Forms/validationProductInput.js
@@ -3,11 +3,11 @@ import { PRODUCT_FORM_KEYS } from "../../util/constants.js";
 export const validationProductInput = (inputName, inputValue) => {
     switch (inputName) {
         case PRODUCT_FORM_KEYS.AMAZON:
-            if (inputValue == '') {
+            if (inputValue === '') {
                 return { [inputName]: 'Amazon Url is required' };
             }
         case PRODUCT_FORM_KEYS.EBAY:
-            if (inputValue == '') {
+            if (inputValue === '') {
                 return { [inputName]: 'Ebay Url is required' };
             }
         default:
