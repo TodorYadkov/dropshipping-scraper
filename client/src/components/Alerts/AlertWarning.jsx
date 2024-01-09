bf9fbc50de83a65f4239e8ef2bbdafbcf4ba2f31
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:11:00 +0200

    Add exit sign

--- client/src/components/Alerts/AlertWarning.jsx
+++ client/src/components/Alerts/AlertWarning.jsx
@@ -21,6 +21,18 @@ export const AlertWarning = () => {
 					</p>
 				</div>
 			</div>
+
+			<div className="cursor-pointer ml-auto mr-2 mt-2" >
+				<svg
+					className="text-black fill-current"
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 18 18"
+				>
+					<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
+				</svg>
+			</div>
 		</div>
 	);
 };
