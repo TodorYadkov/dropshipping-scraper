05c99917c4910a5ca8f73506f83b059cb913c18b
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:19:53 +0200

    Create statisticService

---
+++ client/src/services/statisticService.js
@@ -0,0 +1,9 @@
+import { SERVER_PATHS } from '../util/paths.js';
+
+export const statisticService = (api) => {
+	const getGeneralStatistic = async () => api.get(SERVER_PATHS.GET_GENERAL_STATISTICS);
+
+	return {
+		getGeneralStatistic
+	};
+};
