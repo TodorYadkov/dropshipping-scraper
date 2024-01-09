0d3dce2de50497dc977981889c07ddd6fe8c40b0
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:21:16 +0200

    Add new path on server

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,6 @@
 import { homeController } from '../controllers/homeController.js';
 import { productController } from '../controllers/productController.js';
+import { statisticController } from '../controllers/statisticController.js';
 import { userController } from '../controllers/userController.js';
 import logRequests from '../middlewares/displayRequest.js';
 
@@ -8,6 +9,7 @@ export default (app) => {
 	app.use('/', homeController);
 	app.use('/products', productController);
 	app.use('/users', userController);
+	app.use('/statistics', statisticController);
 	app.all('*', (req, res, next) => {
 		try {
 			throw new Error(`No content - path ${req.path} of method ${req.method} not found`);
