abaa1597085789df738e03c3d6237e9d5796a9be
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:11:24 +0200

    Formatting

--- client/src/components/Buttons/ButtonPrimary.jsx
+++ client/src/components/Buttons/ButtonPrimary.jsx
@@ -1,6 +1,9 @@
-export const ButtonPrimary = ({title, toggle}) => {
+export const ButtonPrimary = ({ title, toggle }) => {
 	return (
-		<button onClick={toggle} className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
+		<button
+			onClick={toggle}
+			className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
+		>
 			{title}
 		</button>
 	);
