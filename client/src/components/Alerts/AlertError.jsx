fbd11f6785ad90987cba467eafb61fa37b2f5055
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:10:45 +0200

    Add exit sign

--- client/src/components/Alerts/AlertError.jsx
+++ client/src/components/Alerts/AlertError.jsx
@@ -1,6 +1,6 @@
-export const AlertError = () => {
+export const AlertError = ({ message, close }) => {
 	return (
-		<div className="inline-flex w-full max-w-sm ml-3 overflow-hidden bg-white rounded-lg shadow-md">
+		<div className="flex w-full max-w-sm ml-3 overflow-hidden bg-white rounded-lg shadow-md">
 			<div className="flex items-center justify-center w-12 bg-red-500">
 				<svg
 					className="w-6 h-6 text-white fill-current"
@@ -15,9 +15,21 @@ export const AlertError = () => {
 				<div className="mx-3">
 					<span className="font-semibold text-red-500">Error</span>
 					<p className="text-sm text-gray-600">
-						Your email is already used!
+						{message}
 					</p>
 				</div>
+			</div>
+
+			<div className="cursor-pointer ml-auto mr-2 mt-2" onClick={close}>
+				<svg
+					className="text-black fill-current"
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 18 18"
+				>
+					<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
+				</svg>
 			</div>
 		</div>
 	);
