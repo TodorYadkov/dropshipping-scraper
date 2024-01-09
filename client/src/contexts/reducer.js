fe326c7e2611b16ce4cba7b8c83385b4a906b4e4
Todor Yadkov <toni_12@abv.bg>
2024-01-09 23:16:22 +0200

    Add new case

--- client/src/contexts/reducer.js
+++ client/src/contexts/reducer.js
@@ -4,10 +4,16 @@ export const reducer = (state, action) => {
 	switch (action.type) {
 		case REDUCER_TYPES.IS_SIDE_BAR_OPEN:
 			return { ...state, [action.type]: action.value };
+
 		case REDUCER_TYPES.PRODUCTS:
 			return { ...state, [action.type]: action.value };
+
 		case REDUCER_TYPES.ADD_PRODUCT:
 			return { ...state, [action.type]: state[action.type].push(action.value) };
+
+		case REDUCER_TYPES.GENERAL_STATISTIC:
+			return { ...state, [action.type]: { ...state[action.type], ...action.value } };
+
 		default:
 			throw new Error('Unrecognized reducer type');
 	}
