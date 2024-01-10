2b16057095d22a6a9aac246a064f412b9c1a21b3
Todor Yadkov <toni_12@abv.bg>
2024-01-10 17:11:37 +0200

    Add new position on tooltip

--- client/src/components/Tooltip.jsx
+++ client/src/components/Tooltip.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
+// To use this component: On parent element add this tailwind styles:  relative group
 export const Tooltip = ({ message, direction = 'center', customTailwindClass }) => {
     const [currentDirection] = useState(() => {
         switch (direction) {
             case 'left':
-                return 'right-0'
+                return 'bottom-full right-0'
             case 'center':
-                return 'left-1/2 -translate-x-1/2'
+                return 'bottom-full left-1/2 -translate-x-1/2'
             case 'right':
-                return 'left-full'
+                return 'bottom-full left-full'
+            case 'bottom':
+                return 'top-full left-1/2 -translate-x-1/2'
         }
     });
 
     const c = customTailwindClass || '';
 
     return (
-        <span className={`${currentDirection} tooltip-text bottom-full absolute z-10 invisible group-hover:visible bg-gray-900 text-white text-xs whitespace-nowrap rounded-lg py-1 px-2${c && ' ' + c}`} >
+        <span className={`${currentDirection} tooltip-text absolute z-10 invisible group-hover:visible bg-gray-900 text-white text-xs whitespace-nowrap rounded-lg py-1 px-2${c && ' ' + c}`} >
             {message}
         </span>
     );
