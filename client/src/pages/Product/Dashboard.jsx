0d6005dd78ca1d162772c21a7e366bf68202ce43
Todor Yadkov <toni_12@abv.bg>
2024-01-10 15:36:49 +0200

    Remove add button

--- client/src/pages/Product/Dashboard.jsx
+++ client/src/pages/Product/Dashboard.jsx
@@ -3,7 +3,6 @@ import { useApi } from '../../hooks/useApi.js';
 
 import { REDUCER_TYPES, TABLE_BODY_TYPES } from '../../util/constants.js';
 
-import { useModal } from '../../hooks/useModal.js';
 import { productService } from '../../services/productService.js';
 import { statisticService } from '../../services/statisticService.js';
 import { useAppStateContext } from '../../hooks/useAppStateContext.js';
@@ -13,14 +12,11 @@ import { Loader } from '../../components/Loader.jsx';
 import { Table } from '../../components/Tables/Table.jsx';
 import { AlertError } from '../../components/Alerts/AlertError.jsx';
 import { DashboardSummary } from '../../components/DashboardSummary.jsx';
-import { ButtonPrimary } from '../../components/Buttons/ButtonPrimary.jsx';
-import { AddProductModal } from '../../components/Modal/AddProductModal.jsx';
 
 export const Dashboard = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [serverError, setServerError] = useState('');
 
-	const [productModal, toggleProductModal] = useModal();
 	const [_] = useIntervalTimeToReceiveData(fetchProductsHandler);
 
 	const { getProducts } = useApi(productService);
@@ -45,7 +41,6 @@ export const Dashboard = () => {
 	// Fetch products from server
 	async function fetchProductsHandler() {
 		try {
-			// TODO: How to make request to get statistic from the server on each request and to follow state on extension or on mount
 			const [products, generalStatistic] = await Promise.all([
 				getProducts(),
 				getGeneralStatistic()
@@ -76,10 +71,7 @@ export const Dashboard = () => {
 					<>
 						<DashboardSummary {...appState[REDUCER_TYPES.GENERAL_STATISTIC]} />
 
-						<ButtonPrimary title="Add Product" toggle={toggleProductModal} />
-						{productModal && <AddProductModal toggleModal={toggleProductModal} />}
-
-						{<Table data={appState[REDUCER_TYPES.PRODUCTS]} type={TABLE_BODY_TYPES.PRODUCT} />}
+						{<Table data={appState[REDUCER_TYPES.PRODUCTS]} typeBody={TABLE_BODY_TYPES.PRODUCT} />}
 					</>
 				)
 			}
