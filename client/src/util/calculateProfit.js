d203cdf35c25ab36fefa6233e11f948e9843d71f
Todor Yadkov <toni_12@abv.bg>
2024-01-12 17:11:16 +0200

    Refactoring calculateProfit

--- client/src/util/calculateProfit.js
+++ client/src/util/calculateProfit.js
@@ -1,6 +1,43 @@
-export const calculateProfit = (currencyAmazon, currencyEbay, priceAmazon, priceEbay) => {
+import { EXTERNAL_API_PATHS } from "./paths.js";
 
-    if (currencyAmazon === currencyEbay) return Number(priceEbay).toFixed(2) - Number(priceAmazon).toFixed(2);
+const calculateProfitForProduct = async (product) => {
+    if (product.priceAmazon === null || product.priceEbay === null) {
+        return null;
+    }
 
-    // https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json
-}   
+    if (product.currencyAmazon === product.currencyEbay) {
+        return (parseFloat(product.priceEbay) - parseFloat(product.priceAmazon)).toFixed(2);
+    }
+
+    try {
+        const baseCurrency = product.currencyAmazon === '$' ? 'usd' : product.currencyAmazon.toLocaleLowerCase();
+        const targetCurrency = product.currencyEbay === '$' ? 'usd' : product.currencyEbay.toLocaleLowerCase();
+
+        const response = await fetch(EXTERNAL_API_PATHS.CURRENCY_COURSE(targetCurrency, baseCurrency));
+        const course = await response.json();
+
+        const exchangeRate = course[baseCurrency];
+        const priceInBaseCurrency = exchangeRate * product.priceEbay;
+
+        product.priceEbay = priceInBaseCurrency;
+        product.currencyEbay = product.currencyAmazon;
+
+        const profitInBaseCurrency = priceInBaseCurrency - product.priceAmazon;
+        return parseFloat(profitInBaseCurrency.toFixed(2));
+    } catch (error) {
+        return null;
+    }
+}
+
+export const calculateProfit = async (products) => {
+    if (products.length === 0) {
+        return products;
+    }
+
+    const productsWithProfit = await Promise.all(products.map(async (product) => {
+        const profit = await calculateProfitForProduct(product);
+        return { ...product, profit };
+    }));
+
+    return productsWithProfit;
+}
