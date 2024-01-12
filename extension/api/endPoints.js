3162a01f3ce915bbf598fc83e0d61249f24e04d1
Todor Yadkov <toni_12@abv.bg>
2024-01-12 13:35:32 +0200

    Change extension default path

--- extension/api/endPoints.js
+++ extension/api/endPoints.js
@@ -1,5 +1,5 @@
-export const HOST = 'http://localhost:3000/';
-// export const HOST = 'https://dropshipping-scraper.vercel.app/';
+// export const HOST = 'http://localhost:3000/';
+export const HOST = 'https://dropshipping-scraper.vercel.app/';
 
 export const ENDPOINTS = {
     GET_PRODUCT: 'products/extension/get-one',
