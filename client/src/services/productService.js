ac25b7be7bcf5d42d06825e8fa9d91fcf3c673e7
Todor Yadkov <toni_12@abv.bg>
2024-01-14 00:30:19 +0200

    Add new function services

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -5,8 +5,14 @@ export const productService = (api) => {
 
 	const createProduct = async (data) => api.post(SERVER_PATHS.CREATE_PRODUCT, data);
 
+	const updateProduct = async (data, productId) => api.put(SERVER_PATHS.UPDATE_PRODUCT(productId), data);
+
+	const deleteProduct = async (productId) => api.delete(SERVER_PATHS.DELETE_PRODUCT(productId));
+
 	return {
 		getProducts,
-		createProduct
+		createProduct,
+		updateProduct,
+		deleteProduct,
 	};
 };
