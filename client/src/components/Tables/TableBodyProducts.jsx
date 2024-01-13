9817886d0250a839b835cfcea2ce5fe42b674916
Todor Yadkov <toni_12@abv.bg>
2024-01-14 00:28:16 +0200

    Add delete functionality and ebay url link add

--- client/src/components/Tables/TableBodyProducts.jsx
+++ client/src/components/Tables/TableBodyProducts.jsx
@@ -6,10 +6,25 @@ import { formatDateToTimeAgo } from "../../util/formatDateToTimeAgo.js";
 import { Tooltip } from "../Tooltip.jsx";
 import { AddProductModal } from "../Modal/AddProductModal.jsx";
 import { AddEbayProductModal } from "../Modal/AddEbayProductModal.jsx";
+import { useState } from "react";
+import { DeleteProductModal } from "../Modal/DeleteProductModal.jsx";
 
 export const TableBodyProducts = ({ products }) => {
+    const [currentProduct, setCurrentProduct] = useState({});
+
     const [productModal, toggleProductModal] = useModal();
     const [ebayProductModal, toggleEbayProductModal] = useModal();
+    const [deleteProductModal, toggleDeleteProductModal] = useModal();
+
+    const ebayModalHandler = (product) => {
+        setCurrentProduct(product);
+        toggleEbayProductModal();
+    };
+
+    const deleteModalHandler = (product) => {
+        setCurrentProduct(product);
+        toggleDeleteProductModal();
+    };
 
     return (
         <tbody className="cursor-default">
@@ -96,14 +111,21 @@ export const TableBodyProducts = ({ products }) => {
                     </td>
                     <td className="px-5 py-5 text-sm border-b border-gray-200 w-1/12">
                         {product.priceEbay ? (
-                            <Link to={product.ebayUrl} target="_blank" rel="noopener noreferrer">
-                                <p className="text-gray-900 whitespace-wrap text-center hover:text-indigo-500 relative group">
-                                    {product.priceEbay && `${product.priceEbay.toFixed(2)} ${product.currencyEbay}`}
-                                    <Tooltip message={'Open eBay Product'} />
-                                </p>
-                            </Link>
+                            <>
+                                <Link to={product.ebayUrl} target="_blank" rel="noopener noreferrer">
+                                    <p className="text-gray-900 whitespace-wrap text-center hover:text-indigo-500 relative group">
+                                        {product.priceEbay && `${product.priceEbay.toFixed(2)} ${product.currencyEbay}`}
+                                        <Tooltip message={'Open eBay Product'} />
+                                    </p>
+                                </Link>
+                                {product?.currencyEbayOriginal && (
+                                    <p className='italic text-xs text-center cursor-default relative group'>({`${product.priceEbayOriginal.toFixed(2)} ${product.currencyEbayOriginal}`})
+                                        <Tooltip message={'Original eBay Price'} direction="bottom" customTailwindClass="not-italic" />
+                                    </p>
+                                )}
+                            </>
                         ) : (
-                            <p className="text-gray-900 whitespace-wrap text-center hover:text-indigo-500 cursor-cell relative group" onClick={toggleEbayProductModal}>
+                            <p className="text-gray-900 whitespace-wrap text-center hover:text-indigo-500 cursor-cell relative group" onClick={() => ebayModalHandler(product)}>
                                 No Price
                                 <Tooltip message={'Add eBay Product'} />
                             </p>
@@ -183,6 +205,7 @@ export const TableBodyProducts = ({ products }) => {
                                 <svg
                                     className="block w-6 h-6 fill-red-600 cursor-pointer p-1 hover:opacity-70"
                                     viewBox="0 0 448 512"
+                                    onClick={() => deleteModalHandler(product)}
                                 >
                                     <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                                 </svg>
@@ -195,7 +218,8 @@ export const TableBodyProducts = ({ products }) => {
             }
 
             {productModal && <tr><td><AddProductModal toggleModal={toggleProductModal} /></td></tr>}
-            {ebayProductModal && <tr><td><AddEbayProductModal toggleModal={toggleEbayProductModal} /></td></tr>}
+            {ebayProductModal && <tr><td><AddEbayProductModal toggleModal={toggleEbayProductModal} product={currentProduct} /></td></tr>}
+            {deleteProductModal && <tr><td><DeleteProductModal toggleModal={toggleDeleteProductModal} product={currentProduct} /></td></tr>}
         </tbody >
     );
 };
