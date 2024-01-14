807cb5c6e38cb1de62c1297c276d14de3963a993
Todor Yadkov <toni_12@abv.bg>
2024-01-14 19:27:23 +0200

    Refactoring and optimize to make only initial request and on new currency

--- client/src/util/calculateProfit.js
+++ client/src/util/calculateProfit.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-prototype-builtins */
 import { EXTERNAL_API_PATHS } from "./paths.js";
 
-const calculateProfitForProduct = async (product) => {
+const calculateProfitForProduct = (product, currentExchangeRate) => {
     if (product.priceAmazon === null || product.priceEbay === null) {
         return null;
     }
@@ -9,39 +10,90 @@ const calculateProfitForProduct = async (product) => {
         return (parseFloat(product.priceEbay) - parseFloat(product.priceAmazon)).toFixed(2);
     }
 
-    try {
-        const baseCurrency = product.currencyAmazon === '$' ? 'usd' : product.currencyAmazon.toLocaleLowerCase();
-        const targetCurrency = product.currencyEbay === '$' ? 'usd' : product.currencyEbay.toLocaleLowerCase();
+    const exchangeRate = currentExchangeRate[product.currencyAmazon.toLocaleLowerCase()];
+    const priceInBaseCurrency = exchangeRate * product.priceEbay;
 
-        const response = await fetch(EXTERNAL_API_PATHS.CURRENCY_COURSE(targetCurrency, baseCurrency));
-        const course = await response.json();
+    product.priceEbayOriginal = product.priceEbay;
+    product.currencyEbayOriginal = product.currencyEbay;
 
-        const exchangeRate = course[baseCurrency];
-        const priceInBaseCurrency = exchangeRate * product.priceEbay;
+    product.priceEbay = priceInBaseCurrency;
+    product.currencyEbay = product.currencyAmazon;
 
-        product.priceEbayOriginal = product.priceEbay;
-        product.currencyEbayOriginal = product.currencyEbay;
+    const profitInBaseCurrency = priceInBaseCurrency - product.priceAmazon;
 
-        product.priceEbay = priceInBaseCurrency;
-        product.currencyEbay = product.currencyAmazon;
-
-        const profitInBaseCurrency = priceInBaseCurrency - product.priceAmazon;
-        return parseFloat(profitInBaseCurrency.toFixed(2));
-        
-    } catch (error) {
-        return null;
-    }
+    return parseFloat(profitInBaseCurrency.toFixed(2));
 }
 
-export const calculateProfit = async (products) => {
+export const calculateProfit = async (products, exchangeRates) => {
     if (products.length === 0) {
         return products;
     }
 
-    const productsWithProfit = await Promise.all(products.map(async (product) => {
-        const profit = await calculateProfitForProduct(product);
+    const productsWithProfit = products.map(product => {
+        const ebayCurrencyExchangeRate = exchangeRates[product?.currencyEbay?.toLocaleLowerCase()];
+        if (!ebayCurrencyExchangeRate) {
+            return { ...product, profit: null };
+        }
+
+        const currentExchangeRate = ebayCurrencyExchangeRate[product.currencyEbay.toLocaleLowerCase()];
+        const profit = calculateProfitForProduct(product, currentExchangeRate);
         return { ...product, profit };
-    }));
+    });
 
     return productsWithProfit;
 }
+
+// Memoized function for currencyCourses calculation
+export const memoizedCalculateCurrencyCourses = async (
+    products,
+    productExchangeCourse,
+    setProductExchangeCourseHandler,
+    addAlertMessage
+) => {
+    try {
+        // Determine main currencies from products
+        const mainCurrencies = new Set(
+            products.map((p) => p.currencyEbay ? p.currencyEbay.toLocaleLowerCase() : 'usd')
+        );
+
+        // Initialize an object to store all available currencies
+        let allAvailableCurrencies = {};
+
+        // Fetch all available currencies if not already in the state
+        if (!productExchangeCourse['allAvailableCurrencies']) {
+            const response = await fetch(EXTERNAL_API_PATHS.CURRENCY_COURSE_ALL);
+            allAvailableCurrencies = await response.json();
+
+            // Update the productExchangeCourse state with all available currencies
+            setProductExchangeCourseHandler({ ...productExchangeCourse, allAvailableCurrencies });
+        }
+
+        // Check and fetch currency courses for main currencies not in the state
+        const newCurrencyCourses = {};
+
+        for (const currency of mainCurrencies) {
+            if (!(currency in productExchangeCourse)) {
+                const currencyExists = allAvailableCurrencies[currency] ||
+                    (productExchangeCourse['allAvailableCurrencies'] && productExchangeCourse['allAvailableCurrencies'][currency]);
+
+                if (currencyExists) {
+                    // Fetch and state currency course if not available
+                    const response = await fetch(EXTERNAL_API_PATHS.CURRENCY_COURSE_TO_AMAZON_CURRENCY(currency));
+                    const currentExchangeRates = await response.json();
+
+                    // Cache the result
+                    newCurrencyCourses[currency] = currentExchangeRates;
+
+                    // Update the productExchangeCourse state with the new state values
+                    setProductExchangeCourseHandler({ ...productExchangeCourse, ...newCurrencyCourses });
+                }
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
+        addAlertMessage(error.message);
+    }
+};
+
+
