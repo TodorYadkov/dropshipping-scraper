3f5785330e46488f88f05b56f8b2ceaf9830a3af
Todor Yadkov <toni_12@abv.bg>
2024-01-15 20:05:34 +0200

    Memoized AddEbayProductModal

--- client/src/components/Modal/AddEbayProductModal.jsx
+++ client/src/components/Modal/AddEbayProductModal.jsx
@@ -1,9 +1,13 @@
+import { memo } from "react";
+
 import { AddEbayUrlForm } from "../Forms/AddEbayUrlForm.jsx";
 import { Modal } from "./Modal.jsx";
 
-export const AddEbayProductModal = ({ toggleModal, product }) => {
+export const AddEbayProductModal = memo(({ toggleModal, product }) => {
 
     return (
         <Modal title={'Add eBay URL'} Content={() => <AddEbayUrlForm product={product} toggleModal={toggleModal} />} toggleModal={toggleModal} backdrop={false} />
     );
-};
+});
+
+AddEbayProductModal.displayName = 'AddEbayProductModal';
