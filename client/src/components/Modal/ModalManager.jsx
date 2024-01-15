82b627837ade757387bc70466fa1421b37bf0c19
Todor Yadkov <toni_12@abv.bg>
2024-01-15 20:06:18 +0200

    Fix problem with rerender modal

---
+++ client/src/components/Modal/ModalManager.jsx
@@ -0,0 +1,16 @@
+import { AddEbayProductModal } from "./AddEbayProductModal.jsx";
+import { AddProductModal } from "./AddProductModal.jsx";
+import { DeleteProductModal } from "./DeleteProductModal.jsx";
+import { EditProductModal } from "./EditProductModal.jsx";
+
+export const ModalManager = ({ useModal, data, closeModal }) => {
+
+    return (
+        <>
+            {useModal === 'AddProductModal' && <AddProductModal toggleModal={closeModal} />}
+            {useModal === 'AddEbayProductModal' && <AddEbayProductModal toggleModal={closeModal} product={data} />}
+            {useModal === 'EditProductModal' && <EditProductModal toggleModal={closeModal} product={data} />}
+            {useModal === 'DeleteProductModal' && <DeleteProductModal toggleModal={closeModal} product={data} />}
+        </>
+    );
+};
