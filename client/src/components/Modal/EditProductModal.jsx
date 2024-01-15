bb6bb2a0ea09323fdf021189e41ddf9a5e55c661
Todor Yadkov <toni_12@abv.bg>
2024-01-15 20:05:59 +0200

    Memoized EditProductModal

--- client/src/components/Modal/EditProductModal.jsx
+++ client/src/components/Modal/EditProductModal.jsx
@@ -1,9 +1,12 @@
+import { memo } from "react";
+
 import { EditProductForm } from "../Forms/EditProductForm.jsx";
 import { Modal } from "./Modal.jsx";
 
-export const EditProductModal = ({ toggleModal, product }) => {
-
+export const EditProductModal = memo(({ toggleModal, product }) => {
     return (
         <Modal title={'Edit product'} Content={() => <EditProductForm product={product} toggleModal={toggleModal} />} toggleModal={toggleModal} backdrop={false} />
     );
-};
+});
+
+EditProductModal.displayName = 'EditProductModal';
