c5d65998ed11411f0d5163f626a89b20db886e7d
Todor Yadkov <toni_12@abv.bg>
2024-01-15 20:05:42 +0200

    Memoized DeleteProductModal

--- client/src/components/Modal/DeleteProductModal.jsx
+++ client/src/components/Modal/DeleteProductModal.jsx
@@ -1,9 +1,13 @@
+import { memo } from "react";
+
 import { DeleteProductForm } from "../Forms/DeleteProductForm.jsx";
 import { Modal } from "./Modal.jsx";
 
-export const DeleteProductModal = ({ toggleModal, product }) => {
+export const DeleteProductModal = memo(({ toggleModal, product }) => {
 
     return (
-        <Modal title={'Delete product'} Content={() => <DeleteProductForm product={product} toggleModal={toggleModal} />} toggleModal={toggleModal} backdrop={false} />
+        <Modal title={'Delete product'} Content={() => <DeleteProductForm product={product} toggleModal={toggleModal} />} toggleModal={toggleModal} />
     );
-};
+});
+
+DeleteProductModal.displayName = 'DeleteProductModal';
