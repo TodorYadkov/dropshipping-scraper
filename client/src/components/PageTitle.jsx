8911fac8425095c595f7efa8446461e1495155d4
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-17 11:48:07 +0200

    Add component PageTitle

---
+++ client/src/components/PageTitle.jsx
@@ -0,0 +1,15 @@
+import { useDocumentTitle } from "../hooks/useDocumentTitle.js";
+
+
+export const PageTitle = ({ title, children }) => {
+
+    const titlePrefix = 'Amazon Scraper | '
+
+    useDocumentTitle(`${titlePrefix}${title}`);
+
+    return (
+        <>
+            {children}
+        </>
+    );
+};
