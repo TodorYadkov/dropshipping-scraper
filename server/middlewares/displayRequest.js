55c28d64229c810c7d8d939b38b59a83f274200e
Todor Yadkov <toni_12@abv.bg>
2024-01-17 21:03:54 +0200

    Turn off all console log of the server

--- server/middlewares/displayRequest.js
+++ server/middlewares/displayRequest.js
@@ -3,11 +3,11 @@ export default () => (req, res, next) => {
     console.log(`Request to: ${req.path} with method: ${req.method}`)
 
     // If there is information in the body - show it
-    const isEmpty = Object.keys(req.body).length === 0;  // TODO remove when we have real users 
-    if (isEmpty === false) {
-        console.log('From: server/middlewares/displayRequest.js: TODO remove when we have real users');
-        console.log(req.body);
-    }
+    // const isEmpty = Object.keys(req.body).length === 0;  // TODO remove when we have real users 
+    // if (isEmpty === false) {
+    //     console.log('From: server/middlewares/displayRequest.js: TODO remove when we have real users');
+    //     console.log(req.body);
+    // }
 
     next();
 }
