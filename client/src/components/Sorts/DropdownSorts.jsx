8b1f8bfcbc025b44f46bd913692e2379aee6bfca
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-17 13:46:49 +0200

    Add logic for dropdownSorts component

--- client/src/components/Sorts/DropdownSorts.jsx
+++ client/src/components/Sorts/DropdownSorts.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const baseValue = 'Ascending by Name';
+
 export const DropdownSorts = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [sort, setSort] = useState(() => {
+        const sortParams = searchParams.get('sort');
+        return sortParams ? sortParams : baseValue;
+    });
+
+    useEffect(() => {
+        setSearchParams((params) => {
+            const paramsObject = Object.fromEntries(params.entries());
+
+            return !paramsObject.hasOwnProperty('sort') ? { page: 1, offset: 5, sort: baseValue } : { ...paramsObject, sort: sort }
+        });
+    }, [sort]);
+
+    const handleSelectChange = (e) => {
+        setSort(e.target.value);
+    };
+
     return (
         <div className="relative">
-            <select className="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white appearance-none border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                <option>All</option>
-                <option>Active</option>
-                <option>Inactive</option>
+            <select
+                className="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white appearance-none border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+                value={sort}
+                onChange={handleSelectChange}
+            >
+                <option value={'Ascending by Name'}>Ascending by Name</option>
+                <option value={'Descending by Name'}>Descending by Name</option>
+                <option value={'Ascending by Amazon price'}>Ascending by Amazon price</option>
+                <option value={'Descending by Amazon price'}>Descending by Amazon price</option>
+                <option value={'Ascending by Profit'}>Ascending by Profit</option>
+                <option value={'Descending by Profit'}>Descending by Profit</option>
+                <option value={'Ascending by Last Updated'}>Ascending by Last Updated</option>
+                <option value={'Descending by Last Updated'}>Descending by Last Updated</option>
             </select>
 
             <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
