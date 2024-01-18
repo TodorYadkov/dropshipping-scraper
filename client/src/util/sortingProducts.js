88fc93e2767318e76bd7de282e2780e9cf7cacd6
Todor Yadkov <toni_12@abv.bg>
2024-01-18 02:51:20 +0200

    Add new string properties

--- client/src/util/sortingProducts.js
+++ client/src/util/sortingProducts.js
@@ -1,28 +1,16 @@
 export const sortingProducts = (sortBy, products) => {
-    switch (sortBy) {
-        case 'Ascending by Name':
-            const test = products.sort((a, b) => a.name.localeCompare(b.name));
-            break;
-        case 'Descending by Name':
-            products.sort((a, b) => b.name.localeCompare(a.name));
-            break;
-        case 'Ascending by Amazon price':
-            products.sort((a, b) => a.priceAmazon - b.priceAmazon);
-            break;
-        case 'Descending by Amazon price':
-            products.sort((a, b) => b.priceAmazon - a.priceAmazon);
-            break;
-        case 'Ascending by Profit':
-            products.sort((a, b) => a.profit - b.profit);
-            break;
-        case 'Descending by Profit':
-            products.sort((a, b) => b.profit - a.profit);
-            break;
-        case 'Ascending by Last Updated':
-            products.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
-            break;
-        case 'Descending by Last Updated':
-            products.sort((a, b) => new Date(a.updatedAt) - new Date(b.updatedAt));
-            break;
-    }
+    let sortFunction = {
+        'name_asc': (a, b) => a.name.localeCompare(b.name),
+        'name_desc': (a, b) => b.name.localeCompare(a.name),
+        'amazon_price_asc': (a, b) => a.priceAmazon - b.priceAmazon,
+        'amazon_price_desc': (a, b) => b.priceAmazon - a.priceAmazon,
+        'ebay_price_asc': (a, b) => a.priceEbay - b.priceEbay,
+        'ebay_price_desc': (a, b) => b.priceEbay - a.priceEbay,
+        'profit_asc': (a, b) => a.profit - b.profit,
+        'profit_desc': (a, b) => b.profit - a.profit,
+        'last_updated_asc': (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt),
+        'last_updated_desc': (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt)
+    };
+
+    products.sort(sortFunction[sortBy] || ((a, b) => a - b));
 }
