24f2d371af5e85bf69db2753071f9ae33c12ad6f
Todor Yadkov <toni_12@abv.bg>
2024-01-18 02:53:57 +0200

    Fix problem when the domain on ebay is different to save in DB with real domain not only with .com

--- server/util/extractItemIdEbay.js
+++ server/util/extractItemIdEbay.js
@@ -10,8 +10,18 @@ export function extractItemIdEbay(url) {
         // If the input is an item ID, return it directly
         return `https://www.ebay.com/itm/${url}`;
     }
-    // If the input is a URL, extract the Item from the URL and return the unified URL
-    const match = url.match(urlRegex);
 
-    return match ? `https://www.ebay.com/itm/${match[1]}` : url;
+    // If the input is a URL, extract the item ID from the URL
+    const match = url.match(urlRegex);
+    if (match) {
+        // Extract the domain from the URL
+        const domainMatch = url.match(/^(https?:\/\/)?([^\/]+)\//);
+        if (domainMatch) {
+            // Construct a new URL with the original domain and modified path
+            return `https://${domainMatch[2]}/itm/${match[1]}`;
+        }
+    }
+
+    // If the input doesn't match either pattern, return the original URL
+    return url;
 }
