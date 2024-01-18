b2a51293f38bbbe3cc396c05422d023161f6d48b
Todor Yadkov <toni_12@abv.bg>
2024-01-18 19:51:10 +0200

    Add new properties

--- client/src/components/Buttons/ButtonPrimary.jsx
+++ client/src/components/Buttons/ButtonPrimary.jsx
@@ -1,9 +1,10 @@
-export const ButtonPrimary = ({ title, toggle }) => {
+export const ButtonPrimary = ({ title, toggle, children }) => {
 	return (
 		<button
 			onClick={toggle}
-			className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
+			className="flex items-center justify-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
 		>
+			{children && children}
 			{title}
 		</button>
 	);
