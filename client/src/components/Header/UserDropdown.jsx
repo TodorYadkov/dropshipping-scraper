4d218f2bb21fae4b6b82a48d9914ad5e2e5231f6
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-19 00:05:05 +0200

    Updated profile image

--- client/src/components/Header/UserDropdown.jsx
+++ client/src/components/Header/UserDropdown.jsx
@@ -16,21 +16,12 @@ export const UserDropdown = () => {
                 className="relative z-5 block w-8 h-8 overflow-hidden rounded-full shadow focus:outline-none"
                 onClick={toggleAvatarDropdown}
             >
-                {/* TODO: add user custom avatar
                         <img
-							className="object-cover w-full h-full"
-							src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=296&q=80"
+							// className="object-cover w-full h-full"
+							className="object-cover w-9/12 h-11/12 ml-1"
+							src="https://res.cloudinary.com/framevibe/image/upload/v1705609288/xfq6pgcrwaybffifd3fk.png"
 							alt="Your avatar"
 						/>
-                    */}
-                <svg
-                    height="16"
-                    width="14"
-                    viewBox="0 0 448 512"
-                    className="object-cover w-9/12 h-11/12 ml-1"
-                >
-                    <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
-                </svg>
             </button>
 
             {avatarDropdownOpen && (
