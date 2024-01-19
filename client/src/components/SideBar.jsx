9c0d56f8d021fa61459cf011543b59ef25383548
Todor Yadkov <toni_12@abv.bg>
2024-01-19 12:06:15 +0200

    Remove unused links

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -68,117 +68,21 @@ export const SideBar = () => {
 
 						<NavLink
 							className={({ isActive }) => setActiveAndInactiveCss(isActive)}
-							to="/ui-elements"
+							to={CLIENT_PATHS.EXTENSIONS}
 						>
 							<svg
 								className="w-5 h-5"
-								viewBox="0 0 20 20"
+								viewBox="-40 0 550 550"
 								fill="none"
 							>
 								<path
-									d="M5 3C3.89543 3 3 3.89543 3 5V7C3 8.10457 3.89543 9 5 9H7C8.10457 9 9 8.10457 9 7V5C9 3.89543 8.10457 3 7 3H5Z"
-									fill="currentColor"
-								/>
-								<path
-									d="M5 11C3.89543 11 3 11.8954 3 13V15C3 16.1046 3.89543 17 5 17H7C8.10457 17 9 16.1046 9 15V13C9 11.8954 8.10457 11 7 11H5Z"
-									fill="currentColor"
-								/>
-								<path
-									d="M11 5C11 3.89543 11.8954 3 13 3H15C16.1046 3 17 3.89543 17 5V7C17 8.10457 16.1046 9 15 9H13C11.8954 9 11 8.10457 11 7V5Z"
-									fill="currentColor"
-								/>
-								<path
-									d="M11 13C11 11.8954 11.8954 11 13 11H15C16.1046 11 17 11.8954 17 13V15C17 16.1046 16.1046 17 15 17H13C11.8954 17 11 16.1046 11 15V13Z"
+									d="M192 104.8c0-9.2-5.8-17.3-13.2-22.8C167.2 73.3 160 61.3 160 48c0-26.5 28.7-48 64-48s64 21.5 64 48c0 13.3-7.2 25.3-18.8 34c-7.4 5.5-13.2 13.6-13.2 22.8c0 12.8 10.4 23.2 23.2 23.2H336c26.5 0 48 21.5 48 48v56.8c0 12.8 10.4 23.2 23.2 23.2c9.2 0 17.3-5.8 22.8-13.2c8.7-11.6 20.7-18.8 34-18.8c26.5 0 48 28.7 48 64s-21.5 64-48 64c-13.3 0-25.3-7.2-34-18.8c-5.5-7.4-13.6-13.2-22.8-13.2c-12.8 0-23.2 10.4-23.2 23.2V464c0 26.5-21.5 48-48 48H279.2c-12.8 0-23.2-10.4-23.2-23.2c0-9.2 5.8-17.3 13.2-22.8c11.6-8.7 18.8-20.7 18.8-34c0-26.5-28.7-48-64-48s-64 21.5-64 48c0 13.3 7.2 25.3 18.8 34c7.4 5.5 13.2 13.6 13.2 22.8c0 12.8-10.4 23.2-23.2 23.2H48c-26.5 0-48-21.5-48-48V343.2C0 330.4 10.4 320 23.2 320c9.2 0 17.3 5.8 22.8 13.2C54.7 344.8 66.7 352 80 352c26.5 0 48-28.7 48-64s-21.5-64-48-64c-13.3 0-25.3 7.2-34 18.8C40.5 250.2 32.4 256 23.2 256C10.4 256 0 245.6 0 232.8V176c0-26.5 21.5-48 48-48H168.8c12.8 0 23.2-10.4 23.2-23.2z"
 									fill="currentColor"
 								/>
 							</svg>
 
-							<span className="mx-4">UI Elements</span>
+							<span className="mx-4">Extensions</span>
 						</NavLink>
-
-						{/* <NavLink
-							className={({ isActive }) => setActiveAndInactiveCss(isActive)}
-							to="/tables"
-						>
-							<svg
-								className="w-5 h-5"
-								viewBox="0 0 20 20"
-								fill="none"
-							>
-								<path
-									d="M7 3C6.44772 3 6 3.44772 6 4C6 4.55228 6.44772 5 7 5H13C13.5523 5 14 4.55228 14 4C14 3.44772 13.5523 3 13 3H7Z"
-									fill="currentColor"
-								/>
-								<path
-									d="M4 7C4 6.44772 4.44772 6 5 6H15C15.5523 6 16 6.44772 16 7C16 7.55228 15.5523 8 15 8H5C4.44772 8 4 7.55228 4 7Z"
-									fill="currentColor"
-								/>
-								<path
-									d="M2 11C2 9.89543 2.89543 9 4 9H16C17.1046 9 18 9.89543 18 11V15C18 16.1046 17.1046 17 16 17H4C2.89543 17 2 16.1046 2 15V11Z"
-									fill="currentColor"
-								/>
-							</svg>
-
-							<span className="mx-4">Tables</span>
-						</NavLink> */}
-
-						<NavLink
-							className={({ isActive }) => setActiveAndInactiveCss(isActive)}
-							to="/forms"
-						>
-							<svg
-								className="w-5 h-5"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-							>
-								<path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-								<path
-									fillRule="evenodd"
-									d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-									clipRule="evenodd"
-								/>
-							</svg>
-
-							<span className="mx-4">Forms</span>
-						</NavLink>
-
-						<NavLink
-							className={({ isActive }) => setActiveAndInactiveCss(isActive)}
-							to="/cards"
-						>
-							<svg
-								className="w-5 h-5"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
-								<path
-									fillRule="evenodd"
-									d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-									clipRule="evenodd"
-								/>
-							</svg>
-
-							<span className="mx-4">Cards</span>
-						</NavLink>
-
-						<NavLink
-							className={({ isActive }) => setActiveAndInactiveCss(isActive)}
-							to="/modal"
-						>
-							<svg
-								className="w-5 h-5"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-								<path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-								<path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-							</svg>
-
-							<span className="mx-4">Modal</span>
-						</NavLink>
-
 					</nav>
 				</div>
 			</div>
