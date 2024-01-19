5cee1c71576030efb0488b2a5ff5406d702d1922
Todor Yadkov <toni_12@abv.bg>
2024-01-19 12:07:07 +0200

    Add new icon on profit

--- client/src/components/Tables/TableBodyProducts.jsx
+++ client/src/components/Tables/TableBodyProducts.jsx
@@ -121,15 +121,24 @@ export const TableBodyProducts = ({ products, onModalClick }) => {
                     </td>
 
                     <td className="px-5 py-5 text-sm border-b border-gray-200 w-1/12">
+
                         {product?.profit ? (
-                            <p className={`${product.profit > 0 ? 'text-gray-900' : 'fill-red-500 text-red-500 flex gap-1 justify-center items-center '} whitespace-wrap text-center relative group`}>
+                            <p className={`${product.profit > 0 ? 'text-green-600 fill-green-600' : 'fill-red-500 text-red-500'} flex gap-1 justify-center items-center whitespace-wrap text-center relative group`}>
                                 {product.profit !== 0 ? `${product.profit} ${product.currencyAmazon}` : '-'}
-                                {product.profit <= 0 && (
+                                {product.profit <= 0 ? (
                                     <svg
                                         className="w-4 h-4"
                                         viewBox="0 0 512 512"
                                     >
-                                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+                                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+                                        {/* <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" /> */}
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        className="w-4 h-4"
+                                        viewBox="0 0 512 512"
+                                    >
+                                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
                                     </svg>
                                 )}
                                 {product.profit >= 0 ? <Tooltip message={'Profit'} /> : <Tooltip message={'The product is sold at a loss!'} customTailwindClass="bg-red-500" />}
