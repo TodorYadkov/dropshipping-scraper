d9fccd9cf38fbe47023aa464abc847b7179fdab2
Todor Yadkov <toni_12@abv.bg>
2024-01-19 12:06:48 +0200

    Add constant file

--- client/src/components/Header/UserDropdown.jsx
+++ client/src/components/Header/UserDropdown.jsx
@@ -43,7 +43,7 @@ export const UserDropdown = () => {
             {avatarDropdownOpen && (
                 <div className="absolute right-0 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl">
                     <Link
-                        to="#"
+                        to={CLIENT_PATHS.PROFILE}
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
                     >
                         Profile
