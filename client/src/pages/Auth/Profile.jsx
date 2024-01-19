ade233b22cb01cb8cb31a91678953f991152fd12
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-19 22:33:07 +0200

    Refactoring css

--- client/src/pages/Auth/Profile.jsx
+++ client/src/pages/Auth/Profile.jsx
@@ -61,74 +61,69 @@ export const Profile = () => {
 
     return (
         <PageTitle title={'Profile'}>
-            <div className="flex-col items-center justify-center w-3/5 mt-8 p-6 mx-auto bg-white rounded-lg overflow-hidden shadow-md cursor-default">
+            <div className="flex-col items-center justify-center w-11/12 sm:w-3/5 mt-8 p-6 mx-auto bg-white rounded-lg overflow-hidden shadow-md cursor-default">
                 <h1 className="text-2xl font-semibold text-center mb-4 text-gray-700">Profile Details</h1>
                 <div className="flex justify-evenly">
 
+                    <div className="w-full max-w-sm p-6 bg-white rounded-md">
+                        <div className="flex items-center justify-center">
 
-                    <div>
-                        <div className="w-full max-w-sm p-6 bg-white rounded-md">
-                            <div className="flex items-center justify-center">
-
-                            </div>
-
-                            {serverError && (
-                                <p className="text-center -mt-6 text-red-600">
-                                    {serverError}
-                                </p>
-                            )}
-
-                            <form className="mt-4" onSubmit={onSubmit} encType="multipart/form-data">
-                                {/* <form className="mt-4" onSubmit={onSubmit} encType={values[AUTH_FORM_KEYS.uploadAvatar] ? "multipart/form-data" : undefined}> */}
-                                <ImageInput
-                                    type="file"
-                                    name={AUTH_FORM_KEYS.uploadAvatar}
-                                    value={values[AUTH_FORM_KEYS.uploadAvatar]}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    error={formErrors[AUTH_FORM_KEYS.uploadAvatar]}
-                                    isEditable={isEditable}
-                                />
-
-                                <Input
-                                    text="Name"
-                                    type="text"
-                                    name={AUTH_FORM_KEYS.name}
-                                    value={values[AUTH_FORM_KEYS.name]}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    error={formErrors[AUTH_FORM_KEYS.name]}
-                                    isEditable={isEditable}
-                                />
-
-                                <Input
-                                    text="Email"
-                                    type="email"
-                                    name={AUTH_FORM_KEYS.email}
-                                    value={values[AUTH_FORM_KEYS.email]}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    error={formErrors[AUTH_FORM_KEYS.email]}
-                                    isEditable={isEditable}
-                                />
-
-                                <div className="mt-6">
-                                    {isLoading ? (
-                                        <Loader width={6} height={6} />
-                                    ) : (
-                                        <input
-                                            type={isEditable ? "submit" : "button"}
-                                            onClick={isEditable ? undefined : changeStateToEditable}
-                                            disabled={isEditable ? isInvalidForm : false}
-                                            value={isEditable ? 'Update' : 'Change Profile'}
-                                            className={`${isEditable ? isInvalidForm ? 'cursor-not-allowed bg-indigo-200' : 'cursor-pointer bg-indigo-600 hover:bg-indigo-400' : 'cursor-pointer bg-indigo-600 hover:bg-indigo-400'} 
-                                            w-full px-4 py-2 text-sm text-center text-white rounded-md focus:outline-none`}
-                                        />
-                                    )}
-                                </div>
-                            </form>
                         </div>
 
+                        {serverError && (
+                            <p className="text-center -mt-6 text-red-600">
+                                {serverError}
+                            </p>
+                        )}
+
+                        <form className="mt-4" onSubmit={onSubmit} encType="multipart/form-data">
+                            <ImageInput
+                                type="file"
+                                name={AUTH_FORM_KEYS.uploadAvatar}
+                                value={values[AUTH_FORM_KEYS.uploadAvatar]}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                error={formErrors[AUTH_FORM_KEYS.uploadAvatar]}
+                                isEditable={isEditable}
+                            />
+
+                            <Input
+                                text="Name"
+                                type="text"
+                                name={AUTH_FORM_KEYS.name}
+                                value={values[AUTH_FORM_KEYS.name]}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                error={formErrors[AUTH_FORM_KEYS.name]}
+                                isEditable={isEditable}
+                            />
+
+                            <Input
+                                text="Email"
+                                type="email"
+                                name={AUTH_FORM_KEYS.email}
+                                value={values[AUTH_FORM_KEYS.email]}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                error={formErrors[AUTH_FORM_KEYS.email]}
+                                isEditable={isEditable}
+                            />
+
+                            <div className="mt-6">
+                                {isLoading ? (
+                                    <Loader width={6} height={6} />
+                                ) : (
+                                    <input
+                                        type={isEditable ? "submit" : "button"}
+                                        onClick={isEditable ? undefined : changeStateToEditable}
+                                        disabled={isEditable ? isInvalidForm : false}
+                                        value={isEditable ? 'Update' : 'Change Profile'}
+                                        className={`${isEditable ? isInvalidForm ? 'cursor-not-allowed bg-indigo-200' : 'cursor-pointer bg-indigo-600 hover:bg-indigo-400' : 'cursor-pointer bg-indigo-600 hover:bg-indigo-400'} 
+                                            w-full px-4 py-2 text-sm text-center text-white rounded-md focus:outline-none`}
+                                    />
+                                )}
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
