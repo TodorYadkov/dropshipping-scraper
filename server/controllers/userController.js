aac65f5ea47b2e3d7bc424d0cbd284e9908048c7
Todor Yadkov <toni_12@abv.bg>
2024-01-19 16:54:49 +0200

    Add new functionality to reset forgot password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
-import { userRegister, userLogin, userLogout, getUserById } from '../services/userService.js'
-import { validateRegisterSchema, validateLoginSchema } from '../util/validationSchemes.js';
+import { userRegister, userLogin, userLogout, getUserById, createResetLink } from '../services/userService.js'
+import { validateRegisterSchema, validateLoginSchema, validateResetPasswordSchema } from '../util/validationSchemes.js';
 import { isUserGuest, isUserLogged } from '../middlewares/guards.js';
 
 const userController = Router();
@@ -38,7 +38,7 @@ userController.post('/login', isUserGuest, async (req, res, next) => {
 userController.get('/logout', isUserLogged, async (req, res, next) => {
     try {
         const userData = { ...req.user, accessToken: req.userToken };
-        
+
         await userLogout(userData);
 
         res.status(200).json({ message: 'Logout successful.' });
@@ -55,6 +55,34 @@ userController.get('/profile', isUserLogged, async (req, res, next) => {
         res.status(200).json(user);
     } catch (err) {
         next(err);
+    }
+});
+
+// Forgot password
+userController.post('/forgot-password', isUserGuest, async (req, res, next) => {
+    try {
+        const userData = req.body;
+        const origin = req.get('Origin');
+        
+        const resetLink = await createResetLink({ ...userData, resetAddress: origin });
+
+        res.status(200).json(resetLink);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Reset password
+userController.post('/reset-password', isUserGuest, async (req, res, next) => {
+    try {
+        const userData = req.body;
+
+        await validateResetPasswordSchema.validateAsync(userData);
+        const resetLink = await (userData);
+
+        res.status(200).json(resetLink);
+    } catch (error) {
+        next(error);
     }
 });
 
