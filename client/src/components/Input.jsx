403d2892a471f6f0ab01737ea98b2f173dce782f
Todor Yadkov <toni_12@abv.bg>
2024-01-20 00:20:43 +0200

    Add placeholder attribute

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,4 +1,4 @@
-export const Input = ({ text, type, name, value, onChange, onBlur, error, isEditable = true }) => {
+export const Input = ({ text, type, name, value, onChange, onBlur, error, isEditable = true, placeholder }) => {
 	return (
 		<label className="block mb-3">
 			<span className="text-sm text-gray-700">{text}</span>
@@ -10,6 +10,7 @@ export const Input = ({ text, type, name, value, onChange, onBlur, error, isEdit
 				onBlur={onBlur}
 				className="block indent-2 w-full mt-1 border border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
 				disabled={!isEditable}
+				placeholder={placeholder}
 			/>
 			{(error.isTouched && !!error.message) && <p className="text-xs -mb-4 text-red-600">{error.message}</p>}
 		</label>
