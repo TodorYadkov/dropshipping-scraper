5ec549fbae160be59775a1f14bee43d9c254e6b5
Todor Yadkov <toni_12@abv.bg>
2024-01-20 00:24:21 +0200

    Add new functionality to send email to reset user password

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,5 +1,9 @@
+import path from 'path';
+import { promises as fsPromises } from 'fs';
+
 import bcrypt from 'bcrypt';
 import nodemailer from 'nodemailer';
+
 
 import { addTokenToBlackList } from './tokenBlackListService.js';
 import { User } from '../models/User.js';
@@ -135,7 +139,7 @@ async function userLogout({ _id, accessToken, isExtension, extensionName }) {
 }
 
 // Create reset link
-async function createResetLink({ email, resetAddress }) {
+async function createResetLink({ email, origin }) {
     // Check if the user with this email exists
     const user = await User.findOne({ email });
     if (!user) {
@@ -144,25 +148,32 @@ async function createResetLink({ email, resetAddress }) {
 
     const temporaryToken = await signJwtToken({ _id: user._id }, { expiresIn: '1h' });
 
+    const encodedToken = encodeURIComponent(temporaryToken);
+
+    const resetLink = `${origin}/reset-password/${encodedToken}`;
+
     // Send an email with the reset link
     const transporter = nodemailer.createTransport({
         // Configure email provider
         service: 'gmail',
         auth: {
             user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASS,
+            pass: process.env.GMAIL_APP_PASS,
         },
     });
 
-    // https://drop-shopper-xi.vercel.app/ TODO
-    const resetLink = `${resetAddress}/${temporaryToken}`;
+    // Construct the file path
+    const templatePath = path.join('html_templates', 'passwordResetTemplate.html');
+    const htmlTemplate = await fsPromises.readFile(templatePath, 'utf8');
+
+    const formattedHtml = htmlTemplate.replace('{{resetLink}}', resetLink);
 
     transporter.sendMail({
-        from: `"Dropshipping Scrapper" ${process.env.GMAIL_USER}`,
+        from: `"Dropshipping Scraper" ${process.env.GMAIL_USER}`,
         to: user.email,
         subject: 'DO NOT REPLY: Dropshipping Scraper - Password Reset',
         text: 'To reset your password, please follow the link. The link is active for 1 hour.',
-        html: `Click <a href="${resetLink}">here</a> to reset your password.`,
+        html: formattedHtml
     });
 
     return { resetLink };
@@ -170,12 +181,13 @@ async function createResetLink({ email, resetAddress }) {
 
 // Reset password
 async function resetUserPassword({ password, resetToken }) {
-    const decodedToken = verifyJwtToken(resetToken);
+    const decodedToken = decodeURIComponent(resetToken);
+    const verifiedToken= verifyJwtToken(decodedToken);
 
     // Hash password
     const hashedPassword = await bcrypt.hash(password, roundsBcrypt);
 
-    const userWithNewPassword = await User.findByIdAndUpdate(decodedToken._id, { password: hashedPassword }, { runValidators: true, new: true });
+    const userWithNewPassword = await User.findByIdAndUpdate(verifiedToken._id, { password: hashedPassword }, { runValidators: true, new: true });
 
     // Create token
     const userToken = await generateUserToken(userWithNewPassword);
@@ -223,3 +235,22 @@ export {
     createResetLink,
     resetUserPassword,
 };
+
+
+
+
+
+
+        //     html: `
+        //     <div>
+        //       <h2 style="color:red">Password Reset</h2>
+        //       <p>
+        //         Hello,<br> 
+        //         To securely reset your password, please open this link within the next hour.
+        //       </p>
+        //       <a href="${resetLink}">Reset Your Password</a>
+        //       <p>
+        //         If you did not request this password reset, you can safely ignore this message.
+        //       </p>
+        //     </div>
+        //   `,
