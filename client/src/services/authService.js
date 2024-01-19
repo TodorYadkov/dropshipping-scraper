7808a7f447d5060c43a37c5e3d67dfc40941c476
Todor Yadkov <toni_12@abv.bg>
2024-01-20 00:22:27 +0200

    Add new request to the server

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,16 +1,22 @@
 import { SERVER_PATHS } from '../util/paths.js';
 
 export const authService = (api) => {
-    
+
 	const login = async (data) => api.post(SERVER_PATHS.LOGIN, data);
 
 	const register = async (data) => api.post(SERVER_PATHS.REGISTER, data);
 
 	const logout = async () => api.get(SERVER_PATHS.LOGOUT);
 
+	const forgotPassword = async (data) => api.post(SERVER_PATHS.FORGOT_PASSWORD, data);
+
+	const resetPassword = async (data) => api.post(SERVER_PATHS.RESET_PASSWORD, data);
+
 	return {
 		login,
 		register,
-		logout
+		logout,
+		forgotPassword,
+		resetPassword,
 	};
 };
