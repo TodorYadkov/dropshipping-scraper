44711e2b63feeb61a9ce4632c22f5489a4d915aa
Todor Yadkov <toni_12@abv.bg>
2024-01-20 02:09:37 +0200

    Change message when the token is invalid

--- server/util/verifyJwtToken.js
+++ server/util/verifyJwtToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export function verifyJwtToken(token) {
     return jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
         if (err) {
-            throw new Error('The token is invalid. Please start the process from beginning.');
+            throw new Error('The token is invalid.');
         }
 
         return decodedToken;
