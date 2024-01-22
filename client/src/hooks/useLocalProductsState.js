a516f4e9996f4c70b27d83faa8363905d22b0a11
Todor Yadkov <toni_12@abv.bg>
2024-01-22 19:47:27 +0200

    Refactoring and dividing responsibility to use only products

--- client/src/hooks/useLocalProductsState.js
+++ client/src/hooks/useLocalProductsState.js
@@ -1,83 +1,32 @@
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { calculateProfit } from '../util/calculateProfit.js';
 import { REDUCER_TYPES } from '../util/constants.js';
-import { sortingProducts } from '../util/sortingProducts.js';
 
 import { useAppStateContext } from './useAppStateContext.js';
+import { useFilterData } from './useFilterData.js';
 
 export const useLocalProductState = (addAlertMessage, exchangeRates) => {
 	const { appState } = useAppStateContext();
-	const [localProducts, setLocalProducts] = useState([]);
-	const [localFilteredState, setLocalFilteredState] = useState({
-		products: [],
-		totalProductCount: 0
-	});
-	const [searchParams] = useSearchParams();
-	
+
+	const [localFilteredState, setLocalProducts] = useFilterData();
+
 	// Set currency to local products
 	useEffect(() => {
 		// Calculate profit
 		setLocalProductsWithSameCurrencyAndProfit();
 	}, [appState[REDUCER_TYPES.PRODUCTS]]);
 
-	// On search filter filter the products
-	useEffect(() => {
-		filterProductsHandler();
-	}, [searchParams]);
 
 	// It set the local products with amazon currency;
 	async function setLocalProductsWithSameCurrencyAndProfit() {
 		try {
 			const productsWithCalculatedCurrencyAndProfit = await calculateProfit(appState[REDUCER_TYPES.PRODUCTS], exchangeRates);
 			setLocalProducts(productsWithCalculatedCurrencyAndProfit);
-			filterProductsHandler(productsWithCalculatedCurrencyAndProfit);
 		} catch (err) {
 			addAlertMessage(err.message);
 			console.error(err);
 		}
-	}
-
-	// Filter the products;
-	function filterProductsHandler(products = localProducts) {
-		let productsToFilter = [...products];
-		let totalProductCount = productsToFilter.length;
-
-		searchHandler(); // Apply search filter
-		sortHandler();
-		pageHandler();  // Slice the products so it contains only products for that page
-
-		// Search
-		function searchHandler() {
-			const search = searchParams.get('search');
-			const searchRegexPattern = new RegExp(search, 'i');
-			if (search) {
-				productsToFilter = productsToFilter.filter(product => searchRegexPattern.test(product.name));
-				totalProductCount = productsToFilter.length;
-			} else {
-				productsToFilter = [...products];
-			}
-		}
-
-		// Slice products for page products
-		function pageHandler() {
-			const offset = Number(searchParams.get('offset')) || 10;
-			const page = Number(searchParams.get('page')) || 1;
-
-			const startIndex = (page - 1) * offset;
-			const endIndex = startIndex + offset;
-
-			productsToFilter = productsToFilter.slice(startIndex, endIndex);
-		}
-
-		// Sorting the products
-		function sortHandler() {
-			const sortBy = searchParams.get('sort');
-			sortBy && sortingProducts(sortBy, productsToFilter);
-		}
-
-		setLocalFilteredState({ totalProductCount, products: productsToFilter });
 	}
 
 	return {
