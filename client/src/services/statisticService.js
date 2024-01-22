7ad94de4f77cc786469c0552671607e846c73e08
Todor Yadkov <toni_12@abv.bg>
2024-01-22 19:52:10 +0200

    Add new endpoint

--- client/src/services/statisticService.js
+++ client/src/services/statisticService.js
@@ -1,9 +1,13 @@
 import { SERVER_PATHS } from '../util/paths.js';
 
 export const statisticService = (api) => {
+
 	const getGeneralStatistic = async () => api.get(SERVER_PATHS.GET_GENERAL_STATISTICS);
 
+	const getExtensionsStatistic = async () => api.get(SERVER_PATHS.GET_EXTENSIONS_STATISTICS);
+
 	return {
-		getGeneralStatistic
+		getGeneralStatistic,
+		getExtensionsStatistic,
 	};
 };
