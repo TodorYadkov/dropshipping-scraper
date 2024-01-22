fe51840edab2a7c0e02e775c7f0f59246b0ded9a
Todor Yadkov <toni_12@abv.bg>
2024-01-23 00:18:53 +0200

    Formatting

--- server/controllers/statisticController.js
+++ server/controllers/statisticController.js
@@ -8,7 +8,7 @@ const statisticController = Router();
 statisticController.get('/general', isUserLogged, async (req, res, next) => {
     try {
         const userId = req.user._id;
-
+        
         const generalStatistic = await getGeneralStatistic(userId);
 
         res.status(200).json(generalStatistic);
