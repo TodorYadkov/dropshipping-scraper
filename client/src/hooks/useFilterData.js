5437977a0646fc43d1de7c0640fc3ef72cdb841d
Todor Yadkov <toni_12@abv.bg>
2024-01-22 19:45:47 +0200

    Divide to new component to be reusable

---
+++ client/src/hooks/useFilterData.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import { sortingData } from "../util/sortingData.js";
+
+export const useFilterData = () => {
+    const [localData, setLocalData] = useState([]);
+    const [localFilteredState, setLocalFilteredState] = useState({
+        data: [],
+        totalDataCount: 0
+    });
+
+    const [searchParams] = useSearchParams();
+
+    // On search filter filter the products
+    useEffect(() => {
+        filterData();
+    }, [searchParams]);
+
+    // Filter the products;
+    function filterData(data = localData) {
+        let dataToFilter = [...data];
+        let totalDataCount = dataToFilter.length;
+
+        searchHandler(); // Apply search filter
+        sortHandler();
+        pageHandler();  // Slice the products so it contains only products for that page
+
+        // Search
+        function searchHandler() {
+            const search = searchParams.get('search');
+            const searchRegexPattern = new RegExp(search, 'i');
+            if (search) {
+                dataToFilter = dataToFilter.filter(d => searchRegexPattern.test(d.name || d.extensionName));
+                totalDataCount = dataToFilter.length;
+            } else {
+                dataToFilter = [...data];
+            }
+        }
+
+        // Slice products for page products
+        function pageHandler() {
+            const offset = Number(searchParams.get('offset')) || 10;
+            const page = Number(searchParams.get('page')) || 1;
+
+            const startIndex = (page - 1) * offset;
+            const endIndex = startIndex + offset;
+
+            dataToFilter = dataToFilter.slice(startIndex, endIndex);
+        }
+
+        // Sorting the products
+        function sortHandler() {
+            const sortBy = searchParams.get('sort');
+            sortBy && sortingData(sortBy, dataToFilter);
+        }
+
+        setLocalFilteredState({ totalDataCount, data: dataToFilter });
+    }
+
+    const setLocalDataHandler = (data) => {
+        setLocalData(data);
+        filterData(data);
+    };
+
+    return [localFilteredState, setLocalDataHandler];
+};
