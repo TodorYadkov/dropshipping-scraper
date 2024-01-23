a05fb0ed5f4ce1e924e51f34f2621d81f92a2ab4
Todor Yadkov <toni_12@abv.bg>
2024-01-23 16:37:49 +0200

    Add new endpoints

--- extension/api/endPoints.js
+++ extension/api/endPoints.js
@@ -2,8 +2,12 @@ export const HOST = 'https://dropshipping-scraper.vercel.app/';
 // export const HOST = 'http://localhost:3000/';
 
 export const ENDPOINTS = {
-    GET_PRODUCT: 'products/extension/get-one',
-    UPDATE_PRODUCT: 'products/extension/put-one',
+    GET_PRODUCT: 'extensions/get-one',
+    UPDATE_PRODUCT: 'extensions/put-one',
+    START_EXTENSION: 'extensions/start',
+    STOP_EXTENSION: 'extensions/stop',
+    STATUS_EXTENSION: 'extensions/status',
+    ERROR_EXTENSION: 'extensions/error',
     LOGIN: 'users/login',
     LOGOUT: 'users/logout',
 }
