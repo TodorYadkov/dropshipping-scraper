5a77a4984813f8a6c155925601e242d112a136ea
Todor Yadkov <toni_12@abv.bg>
2024-01-24 00:31:46 +0200

    Add new function in extensionService

--- server/services/extensionService.js
+++ server/services/extensionService.js
@@ -1,5 +1,40 @@
 import { Product } from '../models/Product.js';
 import { Extension } from '../models/Extension.js';
+import { addTokenToBlackList } from './tokenBlackListService.js';
+
+// EXTENSION REACT request ---------------------
+
+const getAllExtension = (userId) => Extension.find({ owner: userId }).select('-accessToken');
+
+const getOneExtension = async (extensionId) => Extension.findById(extensionId).select('-accessToken');
+
+// LOGOUT extension from React
+const logoutExtensionFromReact = async (userId, extensionId) => {
+    const extension = await Extension.findById(extensionId);
+    if (!extension) {
+        throw new Error('The extension does not exist.')
+    }
+
+    const extensionLogoutData = {
+        userId,
+        extensionName: extension.extensionName,
+        accessToken: extension.accessToken,
+    };
+
+    await addTokenToBlackList(extensionLogoutData);
+
+    extension.isLogin = false;
+    extension.isWork = false;
+    extension.isWorkBrowser = false;
+    extension.accessToken = null;
+
+    await extension.save();
+
+    return extension;
+};
+
+
+// EXTENSION SPECIFIC request ---------------------
 
 // GET ONE(LATEST) product for current user
 const getLatestUpdatedProduct = async (userId) => Product.findOne({ owner: userId }).sort({ updatedAt: 1 });
@@ -17,7 +52,7 @@ const checkExtensionDataInDB = async (userId, extensionName) => {
         { extensionName, owner: userId },
         { isWorkBrowser: true },
         { new: true }
-    );
+    ).select(createExclusionObject());
 
     return extension;
 };
@@ -28,7 +63,7 @@ const startExtension = async (userId, extensionName) => {
         { extensionName, owner: userId },
         { isWork: true },
         { new: true }
-    );
+    ).select(createExclusionObject());
 
     return extension;
 };
@@ -39,7 +74,7 @@ const stopExtension = async (userId, extensionName) => {
         { extensionName, owner: userId },
         { isWork: false },
         { new: true }
-    );
+    ).select(createExclusionObject());
 
     return extension;
 };
@@ -50,12 +85,26 @@ const errorExtension = async (userId, extensionName, error) => {
         { extensionName, owner: userId },
         { error, isWork: false },
         { new: true }
-    );
+    ).select(createExclusionObject());
 
     return extension;
 };
 
+// UTIL FUNCTION  ---------------------
+
+// Exclude properties to return on extension
+function createExclusionObject() {
+    const excludedFields = ['_id', 'extensionName', 'error', 'owner', 'accessToken', 'isWorkBrowser', 'createdAt', 'updatedAt', '__v'];
+    const exclusion = {};
+    excludedFields.forEach(field => (exclusion[field] = 0));
+    return exclusion;
+};
+
+
 export {
+    getAllExtension,
+    getOneExtension,
+    logoutExtensionFromReact,
     getLatestUpdatedProduct,
     updatedProductFromExtension,
     updatedProductOnError,
