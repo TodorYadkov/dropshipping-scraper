3eafb1dd66b0a3b6285a0e6f76acaf74d39be44a
Todor Yadkov <toni_12@abv.bg>
2024-01-23 16:38:49 +0200

    Add new functions

--- extension/services/dataService.js
+++ extension/services/dataService.js
@@ -4,3 +4,11 @@ import { ENDPOINTS } from '../api/endPoints.js';
 export const getLink = () => api.get(ENDPOINTS.GET_PRODUCT);
 
 export const sendData = (data) => api.put(ENDPOINTS.UPDATE_PRODUCT, data);
+
+export const serverStartExtension = () => api.put(ENDPOINTS.START_EXTENSION);
+
+export const serverStopExtension = () => api.put(ENDPOINTS.STOP_EXTENSION);
+
+export const statusExtension = () => api.get(ENDPOINTS.STATUS_EXTENSION);
+
+export const errorExtension = (error) => api.put(ENDPOINTS.ERROR_EXTENSION, error);
