76ae300d15579780f03be1fcad9989aaecfc3d38
Todor Yadkov <toni_12@abv.bg>
2024-01-24 19:02:25 +0200

    Remove unused function

--- client/src/services/statisticService.js
+++ client/src/services/statisticService.js
@@ -4,10 +4,7 @@ export const statisticService = (api) => {
 
 	const getGeneralStatistic = async () => api.get(SERVER_PATHS.GET_GENERAL_STATISTICS);
 
-	const getExtensionsStatistic = async () => api.get(SERVER_PATHS.GET_EXTENSIONS_STATISTICS);
-
 	return {
 		getGeneralStatistic,
-		getExtensionsStatistic,
 	};
 };
