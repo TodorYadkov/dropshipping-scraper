0cbd1112d648233f651b489f1535e3f484fe4a44
Todor Yadkov <toni_12@abv.bg>
2024-01-24 19:04:13 +0200

    Remove unused endpoint

--- server/controllers/statisticController.js
+++ server/controllers/statisticController.js
@@ -1,29 +1,17 @@
 import { Router } from 'express';
 
 import { isUserLogged } from '../middlewares/guards.js';
-import { getExtensionsStatistic, getGeneralStatistic } from '../services/statisticService.js';
+import { getGeneralStatistic } from '../services/statisticService.js';
 
 const statisticController = Router();
 
 statisticController.get('/general', isUserLogged, async (req, res, next) => {
     try {
         const userId = req.user._id;
-        
+
         const generalStatistic = await getGeneralStatistic(userId);
 
         res.status(200).json(generalStatistic);
-    } catch (err) {
-        next(err);
-    }
-});
-
-statisticController.get('/extensions', isUserLogged, async (req, res, next) => {
-    try {
-        const userId = req.user._id;
-
-        const extensionsStatistic = await getExtensionsStatistic(userId);
-
-        res.status(200).json(extensionsStatistic);
     } catch (err) {
         next(err);
     }
