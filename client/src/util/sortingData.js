7cbd9317fe495dcb4af23e266d16f7105d3e1254
Todor Yadkov <toni_12@abv.bg>
2024-01-24 19:03:02 +0200

    Add sorting function for extension

--- client/src/util/sortingData.js
+++ client/src/util/sortingData.js
@@ -1,6 +1,6 @@
 import { SORTING_KEYS } from "./constants.js";
 
-export const sortingData = (sortBy, products) => {
+export const sortingData = (sortBy, data) => {
     const sortFunctions = {
         [SORTING_KEYS.PRODUCT_NAME_ASC]: (a, b) => a.name.localeCompare(b.name),
         [SORTING_KEYS.PRODUCT_NAME_DESC]: (a, b) => b.name.localeCompare(a.name),
@@ -11,8 +11,16 @@ export const sortingData = (sortBy, products) => {
         [SORTING_KEYS.PRODUCT_PROFIT_ASC]: (a, b) => a.profit - b.profit,
         [SORTING_KEYS.PRODUCT_PROFIT_DESC]: (a, b) => b.profit - a.profit,
         [SORTING_KEYS.PRODUCT_LAST_UPDATED_ASC]: (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt),
-        [SORTING_KEYS.PRODUCT_LAST_UPDATED_DESC]: (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt)
+        [SORTING_KEYS.PRODUCT_LAST_UPDATED_DESC]: (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt),
+        [SORTING_KEYS.EXTENSION_NAME_ASC]: (a, b) => a.extensionName.localeCompare(b.extensionName),
+        [SORTING_KEYS.EXTENSION_NAME_DESC]: (a, b) => b.extensionName.localeCompare(a.extensionName),
+        [SORTING_KEYS.EXTENSION_LOGGED_ASC]: (a, b) => b.isLogin - a.isLogin,
+        [SORTING_KEYS.EXTENSION_LOGGED_DESC]: (a, b) => a.isLogin - b.isLogin,
+        [SORTING_KEYS.EXTENSION_WORKING_ASC]: (a, b) => b.isWork - a.isWork,
+        [SORTING_KEYS.EXTENSION_WORKING_DESC]: (a, b) => a.isWork - b.isWork,
+        [SORTING_KEYS.EXTENSION_LAST_SEEN_ASC]: (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt),
+        [SORTING_KEYS.EXTENSION_LAST_SEEN_DESC]: (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt),
     };
 
-    products.sort(sortFunctions[sortBy] || ((a, b) => a - b));
+    data.sort(sortFunctions[sortBy] || ((a, b) => a - b));
 }
