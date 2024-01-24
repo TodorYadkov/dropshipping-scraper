ab0287bc3e52806b2c95c561a21d7566d6b62dbe
Todor Yadkov <toni_12@abv.bg>
2024-01-24 18:56:47 +0200

    Refactoring and optimized to handle errors from different data types and to be standalone component

--- client/src/components/Header/ErrorNotifierDropdown.jsx
+++ client/src/components/Header/ErrorNotifierDropdown.jsx
@@ -1,34 +1,118 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { useAppStateContext } from "../../hooks/useAppStateContext.js";
+import { useApi } from "../../hooks/useApi.js";
+
+import { extensionService } from "../../services/extensionService.js";
+import { productService } from "../../services/productService.js";
 
 import { Tooltip } from "../Tooltip.jsx";
 
-export const ErrorNotifierDropdown = () => {
+export const ErrorNotifierDropdown = memo(() => {
     const [errorDropdownOpen, setErrorDropdownOpen] = useState(false);
-    const [productsWithErrors, setProductWithErrors] = useState([]);
+    const [errors, setErrors] = useState({ productErrors: [], extensionErrors: [] });
     const [countErrors, setCountErrors] = useState(0);
+    const [allData, setAllData] = useState({ products: [], extensions: [] });
 
-    const { appState } = useAppStateContext();
+    const { getProducts } = useApi(productService);
+    const { getAllExtensions } = useApi(extensionService);
 
     useEffect(() => {
+        // Initial Load error
+        (async function () {
+            await requestHandler();
+        })()
 
-        if (appState?.products.some(p => p.error)) {
-            const productErrors = appState.products.filter(p => p.error);
-            setProductWithErrors(productErrors);
-            setCountErrors(productErrors.length);
+        const intervalId = setInterval(async () => {
+            await requestHandler();
 
-        } else {
-            setProductWithErrors([]);
-            setCountErrors(0);
+        }, 180000);
+
+        return () => clearInterval(intervalId);
+
+    }, []);
+
+    useEffect(() => {
+        const productErrors = allData?.products?.filter(p => p?.error) || [];
+        const extensionErrors = allData?.extensions?.filter(e => e?.error) || [];
+
+        const allErrors = [...productErrors, ...extensionErrors];
+
+        setErrors({ productErrors, extensionErrors });
+        setCountErrors(allErrors.length);
+
+    }, [allData.extensions, allData.products]);
+
+    // Show different error
+    const errorHandler = () => {
+        const jsxErrors = [];
+
+        // Errors product
+        if (errors?.productErrors?.length > 0) {
+            jsxErrors.push(
+                ...errors.productErrors.map(p => (
+                    <div key={p._id} className="py-1 px-2 text-xs text-gray-700 hover:bg-indigo-600 hover:text-white relative group">
+                        <p className="truncate w-11/12"><span className="font-bold">Product: </span>{p.name}
+                            <Link to={p.amazonUrl} target="blank" rel="noopener noreferrer">
+                                <svg
+                                    className="absolute top-1 right-2 w-3 h-3 fill-white"
+                                    viewBox="0 0 512 512"
+                                >
+                                    <title>Link Amazon</title>
+                                    <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+                                </svg>
+                            </Link>
+                        </p>
+
+                        <p className="truncate w-full">Error: {p.error}</p>
+                        <Tooltip message={p.error} direction="bottom-left" customTailwindClass="whitespace-pre-wrap w-full h-auto" />
+                    </div >
+                ))
+            );
         }
 
-    }, [appState.products]);
+        // Errors extension
+        if (errors?.extensionErrors?.length > 0) {
+            jsxErrors.push(
+                ...errors.extensionErrors.map(e => (
+                    <div key={e._id} className="py-1 px-2 text-xs text-gray-700 hover:bg-indigo-600 hover:text-white relative group">
+                        <p className="truncate w-11/12"><span className="font-bold">Extension: </span>{e.extensionName}
+                            <svg
+                                className="absolute top-1 right-2 w-3 h-3 fill-white cursor-pointer"
+                                viewBox="0 0 384 512"
+                            >
+                                <title>Remove error</title>
+                                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                            </svg>
+                        </p>
+
+                        <p className="truncate w-full">Error: {e.error}</p>
+                        <Tooltip message={e.error} direction="bottom-left" customTailwindClass="whitespace-pre-wrap w-full h-auto" />
+                    </div>
+                ))
+            );
+        }
+
+        return jsxErrors;
+    };
 
     const toggleErrorDropdown = () => {
         setErrorDropdownOpen(!errorDropdownOpen);
     };
+
+    async function requestHandler() {
+        try {
+            const [products, extensions] = await Promise.all([
+                getProducts(),
+                getAllExtensions()
+            ]);
+
+            setAllData({ products, extensions });
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <div className="relative cursor-default">
@@ -67,24 +151,11 @@ export const ErrorNotifierDropdown = () => {
 
                     {countErrors === 0 && <p className="block px-4 py-2 text-sm text-center text-gray-700">No errors found!</p>}
 
-                    {productsWithErrors.map(p => (
-                        <div key={p._id} className="py-1 px-2 text-xs text-gray-700 hover:bg-indigo-600 hover:text-white relative group">
-                            <Link to={p.amazonUrl} target="blank" rel="noopener noreferrer">
-                                <p title="Link Amazon" className="truncate w-11/12">Product: {p.name}
-                                    <svg
-                                        className="absolute top-1 right-2 w-3 h-3 fill-white"
-                                        viewBox="0 0 512 512">
-                                        <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
-                                    </svg>
-                                </p>
-                            </Link>
-
-                            <p className="truncate w-full">Error: {p.error}</p>
-                            <Tooltip message={p.error} direction="bottom-left" customTailwindClass="whitespace-pre-wrap w-full h-auto" />
-                        </div>
-                    ))}
+                    {countErrors !== 0 && errorHandler()}
                 </div>
             )}
         </div>
     );
-};
+});
+
+ErrorNotifierDropdown.displayName = 'ErrorNotifierDropdown';
