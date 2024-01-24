55b828e183cb2a6564022e1de4ba40cc23c2f0dd
Todor Yadkov <toni_12@abv.bg>
2024-01-25 00:01:56 +0200

    Add new modals

--- client/src/components/Modal/ModalManager.jsx
+++ client/src/components/Modal/ModalManager.jsx
@@ -4,6 +4,10 @@ import { DeleteExtensionModal } from './DeleteExtensionModal.jsx';
 import { DeleteProductModal } from './DeleteProductModal.jsx';
 import { EditExtensionModal } from './EditExtensionModal.jsx';
 import { EditProductModal } from './EditProductModal.jsx';
+import { LogoutExtensionModal } from './LogoutExtensionModal.jsx';
+import { ResetErrorExtensionModal } from './ResetErrorExtensionModal.jsx';
+import { StartExtensionModal } from './StartExtensionModal.jsx';
+import { StopExtensionModal } from './StopExtensionModal.jsx';
 
 export const ModalManager = ({ useModal, data, closeModal }) => {
 
@@ -13,8 +17,16 @@ export const ModalManager = ({ useModal, data, closeModal }) => {
             {useModal === 'AddEbayProductModal' && <AddEbayProductModal toggleModal={closeModal} product={data} />}
             {useModal === 'EditProductModal' && <EditProductModal toggleModal={closeModal} product={data} />}
             {useModal === 'DeleteProductModal' && <DeleteProductModal toggleModal={closeModal} product={data} />}
+
             {useModal === 'DeleteExtensionModal' && <DeleteExtensionModal toggleModal={closeModal} extension={data} />}
             {useModal === 'EditExtensionModal' && <EditExtensionModal toggleModal={closeModal} extension={data} />}
+
+            {useModal === 'ResetErrorExtensionModal' && <ResetErrorExtensionModal toggleModal={closeModal} extension={data} />}
+
+            {useModal === 'StartExtensionModal' && <StartExtensionModal toggleModal={closeModal} extension={data} />}
+            {useModal === 'StopExtensionModal' && <StopExtensionModal toggleModal={closeModal} extension={data} />}
+            {useModal === 'LogoutExtensionModal' && <LogoutExtensionModal toggleModal={closeModal} extension={data} />}
+
         </>
     );
 };
