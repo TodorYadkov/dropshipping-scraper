a32b209226f496d59e176af8ea394eb7d23adaa2
Todor Yadkov <toni_12@abv.bg>
2024-01-25 00:02:40 +0200

    Create StopExtensionModal component

---
+++ client/src/components/Modal/StopExtensionModal.jsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+
+import { AlertStopExtension } from '../Alerts/AlertStopExtension.jsx';
+import { Modal } from './Modal.jsx';
+
+export const StopExtensionModal = memo(({ toggleModal, extension }) => {
+	return (
+		<Modal
+			title={'Stop Extension'}
+			toggleModal={toggleModal}
+			Content={() => (
+				<AlertStopExtension
+					extension={extension}
+					toggleModal={toggleModal}
+				/>
+			)}
+		/>
+	);
+});
+
+StopExtensionModal.displayName = 'StopExtensionModal';
