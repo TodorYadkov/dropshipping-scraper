a1fc200484b10ea3bbdd8f3d823398add7a72fd4
Todor Yadkov <toni_12@abv.bg>
2024-01-25 22:37:34 +0200

    Change dashboard summary to show all available products

--- client/src/components/DashboardSummary.jsx
+++ client/src/components/DashboardSummary.jsx
@@ -6,7 +6,7 @@ export const DashboardSummary = ({
     extensionsIsLogin,
     extensionsNotWorked,
     productsCount,
-    productsErrorCount,
+    availableProductsCount,
 }) => {
 
     return (
@@ -115,7 +115,7 @@ export const DashboardSummary = ({
                                     strokeWidth="2"
                                 />
                             </svg>
-                            <Tooltip message="Available products" direction="bottom" customTailwindClass="mt-1" />
+                            <Tooltip message="All products" direction="bottom" customTailwindClass="mt-1" />
                         </div>
 
                         <div className="mx-5">
@@ -125,25 +125,25 @@ export const DashboardSummary = ({
                             <p className="text-gray-500">Products</p>
                         </div>
 
-                        <div className="p-3 bg-red-600 bg-opacity-75 rounded-full relative group">
-                            <svg 
+                        <div className="p-3 bg-green-600 bg-opacity-75 rounded-full relative group">
+                            <svg
                                 className="w-8 h-8 text-white"
                                 fill="white"
-                                viewBox="0 30 512 512">
-                                <path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
+                                viewBox="0 0 512 512">
+                                <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
                             </svg>
-                            <Tooltip message="Product errors" direction="bottom" customTailwindClass="mt-1" />
+                            <Tooltip message="Available products" direction="bottom" customTailwindClass="mt-1" />
                         </div>
 
                         <div className="mx-5">
                             <h4 className="text-2xl font-semibold text-gray-700">
-                                {productsErrorCount}
+                                {availableProductsCount}
                             </h4>
-                            <p className="text-gray-500">Errors</p>
+                            <p className="text-gray-500">Available</p>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     );
