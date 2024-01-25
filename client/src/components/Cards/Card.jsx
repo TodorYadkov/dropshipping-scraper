80804ade4de92ea4f029822dcd5bf8ff33f12c1b
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-25 18:14:01 +0200

    Add CardExtension for case EXTENSION

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -2,18 +2,19 @@ import { useMemo } from 'react';
 
 import { DATA_TYPES } from '../../util/constants.js';
 import { CardProducts } from './CardProducts.jsx';
+import { CardExtension } from './CardExtension.jsx';
 
 export const Card = ({ dataTypes, data, onModalClick }) => {
 
     const CardVariant = useMemo(() => {
         let CardComponent = null;
-        
+
         switch (dataTypes) {
             case DATA_TYPES.PRODUCT:
                 CardComponent = <CardProducts products={data} onModalClick={onModalClick} />
                 break;
             case DATA_TYPES.EXTENSION:
-                CardComponent = <h1>Test</h1>
+                CardComponent = <CardExtension extensions={data} onModalClick={onModalClick} />
                 break;
             case DATA_TYPES.USER:
                 break;
