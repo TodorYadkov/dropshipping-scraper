86f2d3e7ccf384ac4f3131c1ecf46db6152d56ae
Todor Yadkov <toni_12@abv.bg>
2024-01-25 11:55:44 +0200

    Fix disabled button when the extension is logged

--- client/src/components/Tables/TableExtensionsData.jsx
+++ client/src/components/Tables/TableExtensionsData.jsx
@@ -65,7 +65,14 @@ export const TableExtensionsData = ({ extensionsData, onModalClick }) => {
                                 <svg
                                     viewBox="0 0 30.143 30.143"
                                     className={`w-6 h-6 ${extension.isWork ? 'fill-green-600' : 'fill-red-600'} ${extension.isWorkBrowser ? 'cursor-pointer' : 'cursor-not-allowed opacity-20'}`}
-                                    onClick={extension.isWorkBrowser ? (extension.isWork ? () => onModalClick('StopExtensionModal', { ...extension }) : () => onModalClick('StartExtensionModal', { ...extension })) : undefined}
+                                    onClick={
+                                        extension.isWorkBrowser
+                                            ? (
+                                                extension.isWork
+                                                    ? () => onModalClick('StopExtensionModal', { ...extension })
+                                                    : () => onModalClick('StartExtensionModal', { ...extension })
+                                            ) : undefined
+                                    }
                                 >
                                     <path d="M20.034,2.357v3.824c3.482,1.798,5.869,5.427,5.869,9.619c0,5.98-4.848,10.83-10.828,10.83 c-5.982,0-10.832-4.85-10.832-10.83c0-3.844,2.012-7.215,5.029-9.136V2.689C4.245,4.918,0.731,9.945,0.731,15.801 c0,7.921,6.42,14.342,14.34,14.342c7.924,0,14.342-6.421,14.342-14.342C29.412,9.624,25.501,4.379,20.034,2.357z" />
                                     <path d="M14.795,17.652c1.576,0,1.736-0.931,1.736-2.076V2.08c0-1.148-0.16-2.08-1.736-2.08 c-1.57,0-1.732,0.932-1.732,2.08v13.496C13.062,16.722,13.225,17.652,14.795,17.652z" />
@@ -83,7 +90,11 @@ export const TableExtensionsData = ({ extensionsData, onModalClick }) => {
                                     className={`icon w-6 h-6 fill-indigo-600 ${extension.isWorkBrowser ? 'cursor-pointer' : 'cursor-not-allowed opacity-20'}`}
                                     stroke="#4f46e5"
                                     strokeWidth="50"
-                                    onClick={extension.isWorkBrowser ? () => onModalClick('LogoutExtensionModal', { ...extension }) : undefined}
+                                    onClick={
+                                        extension.isWorkBrowser
+                                            ? () => onModalClick('LogoutExtensionModal', { ...extension })
+                                            : undefined
+                                    }
                                 >
                                     <path d="M868 732h-70.3c-4.8 0-9.3 2.1-12.3 5.8-7 8.5-14.5 16.7-22.4 24.5a353.84 353.84 0 0 1-112.7 75.9A352.8 352.8 0 0 1 512.4 866c-47.9 0-94.3-9.4-137.9-27.8a353.84 353.84 0 0 1-112.7-75.9 353.28 353.28 0 0 1-76-112.5C167.3 606.2 158 559.9 158 512s9.4-94.2 27.8-137.8c17.8-42.1 43.4-80 76-112.5s70.5-58.1 112.7-75.9c43.6-18.4 90-27.8 137.9-27.8 47.9 0 94.3 9.3 137.9 27.8 42.2 17.8 80.1 43.4 112.7 75.9 7.9 7.9 15.3 16.1 22.4 24.5 3 3.7 7.6 5.8 12.3 5.8H868c6.3 0 10.2-7 6.7-12.3C798 160.5 663.8 81.6 511.3 82 271.7 82.6 79.6 277.1 82 516.4 84.4 751.9 276.2 942 512.4 942c152.1 0 285.7-78.8 362.3-197.7 3.4-5.3-.4-12.3-6.7-12.3zm88.9-226.3L815 393.7c-5.3-4.2-13-.4-13 6.3v76H488c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h314v76c0 6.7 7.8 10.5 13 6.3l141.9-112a8 8 0 0 0 0-12.6z" />
                                 </svg>
@@ -123,9 +134,7 @@ export const TableExtensionsData = ({ extensionsData, onModalClick }) => {
                     </td>
 
                 </tr>
-            ))
-            }
-
+            ))}
         </tbody >
     );
 };
