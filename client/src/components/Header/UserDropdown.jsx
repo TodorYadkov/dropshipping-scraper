fc887acaea8f0197eeab7724ef2e610c448b37e6
Todor Yadkov <toni_12@abv.bg>
2024-01-25 11:54:59 +0200

    Fix problem to close dropdown

--- client/src/components/Header/UserDropdown.jsx
+++ client/src/components/Header/UserDropdown.jsx
@@ -38,6 +38,7 @@ export const UserDropdown = memo(() => {
                     <Link
                         to={CLIENT_PATHS.PROFILE}
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
+                        onClick={toggleAvatarDropdown}
                     >
                         Profile
                     </Link>
