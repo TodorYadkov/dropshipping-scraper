49b8fc0b5b6ecf28dd32062b2b79cbfe88ff5769
Todor Yadkov <toni_12@abv.bg>
2024-01-26 21:51:01 +0200

    Fix problem with calculate the profit

--- client/src/hooks/useLocalProductsState.js
+++ client/src/hooks/useLocalProductsState.js
@@ -1,27 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-import { calculateProfit } from '../util/calculateProfit.js';
+import { calculateProfit, loadCurrencyCourses } from '../util/calculateProfit.js';
 import { REDUCER_TYPES } from '../util/constants.js';
 
 import { useAppStateContext } from './useAppStateContext.js';
 import { useFilterData } from './useFilterData.js';
 
-export const useLocalProductState = (addAlertMessage, exchangeRates) => {
+export const useLocalProductState = (addAlertMessage) => {
+	const [productExchangeCourse, setProductExchangeCourse] = useState({});
 	const { appState } = useAppStateContext();
 
 	const [localFilteredState, setLocalProducts] = useFilterData();
 
 	// Set currency to local products
 	useEffect(() => {
-		// Calculate profit
-		setLocalProductsWithSameCurrencyAndProfit();
+		if (appState[REDUCER_TYPES.PRODUCTS].length > 0) {
+			const cachedCurrencies = async () => {
+				const allAvailableCurrenciesOnApi = await loadCurrencyCourses(appState[REDUCER_TYPES.PRODUCTS], productExchangeCourse, setProductExchangeCourse);
+
+				// Calculate profit
+				setLocalProductsWithSameCurrencyAndProfit(allAvailableCurrenciesOnApi);
+			}
+
+			cachedCurrencies();
+		}
+
 	}, [appState[REDUCER_TYPES.PRODUCTS]]);
 
-
 	// It set the local products with amazon currency;
-	async function setLocalProductsWithSameCurrencyAndProfit() {
+	async function setLocalProductsWithSameCurrencyAndProfit(allCurrencies) {
 		try {
-			const productsWithCalculatedCurrencyAndProfit = await calculateProfit(appState[REDUCER_TYPES.PRODUCTS], exchangeRates);
+			const productsWithCalculatedCurrencyAndProfit = await calculateProfit(appState[REDUCER_TYPES.PRODUCTS], { ...allCurrencies, ...productExchangeCourse });
 			setLocalProducts(productsWithCalculatedCurrencyAndProfit);
 		} catch (err) {
 			addAlertMessage(err.message);
