ea6ac471280edc45ad0645d2ec62f773b6093dc6
Todor Yadkov <toni_12@abv.bg>
2024-01-26 19:36:22 +0200

    Add useDataFetcher in layout to optimize fetch request

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,14 +1,23 @@
 import { useAuthContext } from '../hooks/useAuthContext.js';
+import { useDataFetcher } from '../hooks/useDataFetcher.js';
 
 import { Footer } from './Footer.jsx';
 import { Header } from './Header/Header.jsx';
 import { SideBar } from './SideBar.jsx';
+import { AlertError } from './Alerts/AlertError.jsx';
 
 export const Layout = ({ children }) => {
 	const { isAuthenticated } = useAuthContext();
+	const [serverErrorMessage, removeServerErrorMessageHandler] = useDataFetcher();
 
 	return (
 		<div className="flex h-screen bg-gray-200 font-roboto cursor-default">
+
+			{serverErrorMessage && ((
+				<div className="absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+					<AlertError message={serverErrorMessage} close={removeServerErrorMessageHandler} />
+				</div>
+			))}
 
 			{isAuthenticated && <SideBar />}
 
