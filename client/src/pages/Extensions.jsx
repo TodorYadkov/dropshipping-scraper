b7fc844fe7ae97b4f3b5d69d5e090742289d6aab
Todor Yadkov <toni_12@abv.bg>
2024-01-26 19:43:12 +0200

    Refactoring to use useDataFetch

--- client/src/pages/Extensions.jsx
+++ client/src/pages/Extensions.jsx
@@ -1,44 +1,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-import { useApi } from "../hooks/useApi.js";
 import { useFilterData } from "../hooks/useFilterData.js";
 import { useAppStateContext } from "../hooks/useAppStateContext.js";
 import { useIntervalTimeToReceiveData } from "../hooks/useIntervalTimeToReceiveData.js";
 
 import { DATA_TYPES, REDUCER_TYPES } from "../util/constants.js";
 
-import { extensionService } from "../services/extensionService.js";
-
 import { PageTitle } from "../components/PageTitle.jsx";
 import { Loader } from "../components/Loader.jsx";
-import { AlertError } from "../components/Alerts/AlertError.jsx";
 import { ResponsiveComponent } from "../components/ResponsiveComponent.jsx";
 import { DashboardSummary } from "../components/DashboardSummary.jsx";
 
-
 export const Extensions = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [alert, setAlert] = useState('');
-
 	const [extensionsData, setExtensionsLocalStateData] = useFilterData();
-	const [_] = useIntervalTimeToReceiveData(fetchExtensionsData);
+	const [_] = useIntervalTimeToReceiveData(getExtensionsData);
 
-	const { getExtensions } = useApi(extensionService);
-
-	const { appState, setExtensions: setExtensionGlobalStateData } = useAppStateContext();
+	const { appState, setIsLoadingState, setRefreshState } = useAppStateContext();
 
 	// Initial
 	useEffect(() => {
-		async function initialLoading() {
-			setIsLoading(true);
-
-			await fetchExtensionsData();
-			setIsLoading(false);
-		}
-
-		initialLoading();
+		setIsLoadingState(true);
+		getExtensionsData();
 
 	}, []);
 
@@ -48,49 +32,27 @@ export const Extensions = () => {
 
 	}, [appState[REDUCER_TYPES.EXTENSIONS]]);
 
-	// Fetch extension data from server
-	async function fetchExtensionsData() {
-		try {
-			const extensionsData = await getExtensions();
+	// Get extension data from global state
+	function getExtensionsData() {
+		// Start fetch extensions
+		setRefreshState(true);
 
-			setExtensionsLocalStateData(extensionsData);
-			setExtensionGlobalStateData(extensionsData);
+		const extensionsData = appState[REDUCER_TYPES.EXTENSIONS];
 
-		} catch (error) {
-			console.error(error);
-			addAlertMessage(error.message);
-		}
-	}
-
-	function addAlertMessage(error) {
-		setAlert(error);
-	}
-
-	function onCloseAlert() {
-		setAlert('');
-	}
-
-	async function onRefreshClick() {
-		return fetchExtensionsData();
+		setExtensionsLocalStateData(extensionsData);
 	}
 
 	return (
 		<PageTitle title={'Extensions'}>
 			<div className='relative'>
 
-				{isLoading
+				{appState[REDUCER_TYPES.IS_LOADING_STATE]
 					? <Loader />
 					: (
 						<>
 							<DashboardSummary {...appState[REDUCER_TYPES.GENERAL_STATISTIC]} />
 
-							{alert && (
-								<div className="absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5">
-									<AlertError message={alert} close={onCloseAlert} />
-								</div>
-							)}
-
-							<ResponsiveComponent dataType={DATA_TYPES.EXTENSION} localFilteredState={extensionsData} onRefresh={onRefreshClick} />
+							<ResponsiveComponent dataType={DATA_TYPES.EXTENSION} localFilteredState={extensionsData} onRefresh={getExtensionsData} />
 						</>
 					)
 				}
