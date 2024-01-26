01ff3b63722c7284ed93dbad5589e99ec9a67b63
Todor Yadkov <toni_12@abv.bg>
2024-01-26 19:42:09 +0200

    Create useDataFetcher custom hook

---
+++ client/src/hooks/useDataFetcher.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+import { REDUCER_TYPES } from "../util/constants.js";
+
+import { extensionService } from "../services/extensionService.js";
+import { productService } from "../services/productService.js";
+import { statisticService } from "../services/statisticService.js";
+
+import { useApi } from "./useApi.js";
+import { useAppStateContext } from "./useAppStateContext.js";
+
+export const useDataFetcher = () => {
+    const [alert, setAlert] = useState('');
+
+    const { appState, setIsLoadingState, setRefreshState, setGeneralStatistic, setProducts, setExtensions } = useAppStateContext();
+
+    const { getProducts } = useApi(productService);
+    const { getExtensions } = useApi(extensionService);
+    const { getGeneralStatistic } = useApi(statisticService);
+
+    useEffect(() => {
+        (async function () {
+            try {
+                if (appState[REDUCER_TYPES.REFRESH_STATE]) {
+                    const [products, extensions, generalStatistic] = await Promise.allSettled([
+                        getProducts(),
+                        getExtensions(),
+                        getGeneralStatistic()
+                    ]);
+
+                    if (products.status === 'fulfilled') {
+                        setProducts(products.value);
+                    } else {
+                        setAlert(`Error fetching products: ${products.reason.message}`);
+                    }
+
+                    if (extensions.status === 'fulfilled') {
+                        setExtensions(extensions.value);
+                    } else {
+                        setAlert(`Error fetching extensions: ${extensions.reason.message}`);
+                    }
+
+                    if (generalStatistic.status === 'fulfilled') {
+                        setGeneralStatistic(generalStatistic.value);
+                    } else {
+                        setAlert(`Error fetching general statistics: ${generalStatistic.reason.message}`);
+                    }
+
+                    setRefreshState(false);
+                    setIsLoadingState(false);
+                }
+
+            } catch (error) {
+                console.error(error);
+                setAlert(`An unexpected error occurred: ${error.message}`);
+            }
+        })();
+
+    }, [appState[REDUCER_TYPES.REFRESH_STATE]]);
+
+    const clearErrorHandler = () => {
+        setAlert('');
+    };
+
+    return [alert, clearErrorHandler];
+};
