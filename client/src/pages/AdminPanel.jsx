9ca50c8be0dc10e3bd94adc61072bf8c1a493d03
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 22:37:06 +0200

    Implement AdminPanelContext

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -5,21 +5,24 @@ import { adminService } from "../services/adminService.js";
 import { useFilterData } from "../hooks/useFilterData.js";
 import { useIntervalTimeToReceiveData } from "../hooks/useIntervalTimeToReceiveData.js";
 
+import { AdminPanelContext } from "../contexts/AdminPanelContext.jsx";
 import { AlertError } from "../components/Alerts/AlertError.jsx";
 import { ResponsiveComponent } from "../components/ResponsiveComponent.jsx";
 import { PageTitle } from "../components/PageTitle.jsx";
 import { Loader } from "../components/Loader.jsx";
 import { DATA_TYPES } from "../util/constants.js";
 
+
+
 export const AdminPanel = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [alert, setAlert] = useState('');
 
-    const [adminPanelData, setAdminPanelData] = useFilterData();
+    const [adminPanelData, setAdminPanelData, updateUserData] = useFilterData();
     const [_] = useIntervalTimeToReceiveData(fetchAdminPanelData);
-   
+
     const { getAllUsers } = useApi(adminService);
-  
+
     // Initial
     useEffect(() => {
         async function initialLoading() {
@@ -57,24 +60,30 @@ export const AdminPanel = () => {
         return fetchAdminPanelData();
     }
 
+    const values = {
+        updateUserData
+    }
+
     return (
         <PageTitle title={'Admin Panel'}>
-            <div className='relative'>
-                {isLoading
-                    ? <Loader />
-                    : (
-                        <>
-                            {alert && (
-                                <div className="absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5">
-                                    <AlertError message={alert} close={onCloseAlert} />
-                                </div>
-                            )}
+            <AdminPanelContext.Provider value={values}>
+                <div className='relative'>
+                    {isLoading
+                        ? <Loader />
+                        : (
+                            <>
+                                {alert && (
+                                    <div className="absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5">
+                                        <AlertError message={alert} close={onCloseAlert} />
+                                    </div>
+                                )}
 
-                            <ResponsiveComponent dataType={DATA_TYPES.USER} localFilteredState={adminPanelData} onRefresh={onRefreshClick} />
-                        </>
-                    )
-                }
-            </div>
+                                <ResponsiveComponent dataType={DATA_TYPES.USER} localFilteredState={adminPanelData} onRefresh={onRefreshClick} />
+                            </>
+                        )
+                    }
+                </div>
+            </AdminPanelContext.Provider>
         </PageTitle>
     );
 };
