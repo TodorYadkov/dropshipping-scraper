2604ac7fbaadd905a57eab1311322b44b3a24486
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:03:24 +0200

    Add clearAppState ref for decorating httpRequester

--- client/src/hooks/useApi.js
+++ client/src/hooks/useApi.js
@@ -1,9 +1,12 @@
 import { api } from '../api/http-requester.js';
+import { useAppStateContext } from './useAppStateContext.js';
 import { useAuthContext } from './useAuthContext.js';
 
 export const useApi = (service) => {
+	const { clearAppState } = useAppStateContext();
 	const { clearUserState, accessToken } = useAuthContext();
-	const decoratedApi = api(clearUserState, accessToken);
+
+	const decoratedApi = api(clearUserState, clearAppState, accessToken);
 
 	const { ...apiServices } = service(decoratedApi);
 	return apiServices;
