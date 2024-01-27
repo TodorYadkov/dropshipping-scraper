a12804a2ea12c87e46e4a219dd28774d4282c658
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:09:26 +0200

    Add clearAppState on logout and implement PageTitle component

--- client/src/pages/Auth/Logout.jsx
+++ client/src/pages/Auth/Logout.jsx
@@ -5,28 +5,32 @@ import { CLIENT_PATHS } from '../../util/paths.js';
 
 import { useApi } from '../../hooks/useApi.js';
 import { useAuthContext } from '../../hooks/useAuthContext.js';
+import { useAppStateContext } from '../../hooks/useAppStateContext.js';
 
 import { authService } from '../../services/authService.js';
 
 import { Loader } from '../../components/Loader.jsx';
+import { PageTitle } from '../../components/PageTitle.jsx';
 
 export const Logout = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [serverError, setServerError] = useState('');
 
 	const { clearUserState } = useAuthContext();
+	const { clearAppState } = useAppStateContext();
+
 	const { logout } = useApi(authService);
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		document.title = 'Logout';
-
 		(async function onLogout() {
 			try {
 				setIsLoading(true);
 				await logout();
 
+				clearAppState();
 				clearUserState();
+
 				navigate(CLIENT_PATHS.LOGIN, { replace: true });
 			} catch (error) {
 				setServerError(error.message);
@@ -38,13 +42,15 @@ export const Logout = () => {
 	}, []);
 
 	return (
-		<div className="mt-16">
-			{isLoading && <Loader />}
-			{serverError && (
-				<p className="text-center -mb-6 text-red-600">
-					{serverError}
-				</p>
-			)}
-		</div>
+		<PageTitle title={'Logout'}>
+			<div className="mt-16">
+				{isLoading && <Loader />}
+				{serverError && (
+					<p className="text-center -mb-6 text-red-600">
+						{serverError}
+					</p>
+				)}
+			</div>
+		</PageTitle>
 	);
 };
