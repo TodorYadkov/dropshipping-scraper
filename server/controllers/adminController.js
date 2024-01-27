d5e3b017d95f6bc3cd692882456676546bc50cbc
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:23:47 +0200

    Add admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,13 +1,24 @@
 import { Router } from 'express';
-import { getAllUsers } from '../services/adminService.js';
+
+import { getAllUsersDetails } from '../services/adminService.js';
+import { getUserById } from '../services/userService.js';
+import { isUserLogged, isUserRole } from '../middlewares/guards.js';
+import { preload } from '../middlewares/preloader.js';
+
+import { USER_ROLES } from '../environments/userRoles.js';
+import { PRELOAD_OPTIONS } from '../environments/preloadOptions.js';
 
 const adminController = Router();
 
 // Get all users
-adminController.get('/', async (req, res, next) => {
-    const allUsers = await getAllUsers();
+adminController.get('/', isUserLogged, preload(getUserById, PRELOAD_OPTIONS.ROLE), isUserRole(USER_ROLES.ADMIN), async (req, res, next) => {
+    try {
+        const allUsers = await getAllUsersDetails();
 
-    res.status(200).json(allUsers);
+        res.status(200).json(allUsers);
+    } catch (err) {
+        next(err);
+    }
 })
 
 export { adminController };
