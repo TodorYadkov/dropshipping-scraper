96b161d493cfa528291d7d38f3fb968fbce5ac5f
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:22:46 +0200

    Add middleware checkUserIsDisabled

---
+++ server/middlewares/checkUserIsDisabled.js
@@ -0,0 +1,21 @@
+import { getUserById } from "../services/userService.js";
+
+export default () => async (req, res, next) => {
+    const user = req.user;
+
+    if (user) {
+        try {
+            const userFromDb = await getUserById(user._id);
+            if (userFromDb.disable) {
+                throw new Error('Your account has been disabled from admin');
+            }
+            
+        } catch (error) {
+            // Add status code and invoke global error handler
+            error.statusCode = 403;
+            return next(error);
+        }
+    }
+
+    next();
+};
