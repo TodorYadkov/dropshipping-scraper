8043f65b6093f4abee15058027f9b34e7873be8d
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:05:49 +0200

    Add RouterGuardAdmin component

---
+++ client/src/guards/RouterGuardAdmin.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { useAuthContext } from '../hooks/useAuthContext.js';
+import { CLIENT_PATHS } from '../util/paths.js';
+
+export const RouteGuardAdmin = ({ children }) => {
+    const { isAdmin } = useAuthContext();
+
+    if (!isAdmin) {
+        return <Navigate to={CLIENT_PATHS.LOGIN} replace />;
+    }
+
+    return children ? children : <Outlet />;
+};
