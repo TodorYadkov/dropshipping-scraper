1a54cb3987c69da976d3de69d09c262cbaaeb0ee
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:06:11 +0200

    Add constant path

--- client/src/guards/RouteGuardAuthenticated.jsx
+++ client/src/guards/RouteGuardAuthenticated.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 import { useAuthContext } from '../hooks/useAuthContext.js';
+import { CLIENT_PATHS } from '../util/paths.js';
 
 export const RouteGuardAuthenticated = ({ children }) => {
     const { isAuthenticated } = useAuthContext();
 
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace />;
+        return <Navigate to={CLIENT_PATHS.LOGIN} replace />;
     }
 
     return children ? children : <Outlet />;
