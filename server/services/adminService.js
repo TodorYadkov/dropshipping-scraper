4b83e2d5c426a13975f279d48987027d5631c0a9
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:21:31 +0200

    Add adminService

--- server/services/adminService.js
+++ server/services/adminService.js
@@ -1,7 +1,43 @@
 import { User } from "../models/User.js";
 
-const getAllUsers = async () => User.find().select('-password');
+const getAllUsersDetails = async () => User.aggregate([
+    {
+        $lookup: {
+            from: 'products',
+            localField: '_id',
+            foreignField: 'owner',
+            as: 'products'
+        }
+    },
+    {
+        $lookup: {
+            from: 'extensions',
+            localField: '_id',
+            foreignField: 'owner',
+            as: 'extensions'
+        }
+    },
+    {
+        $addFields: {
+            productCount: { $size: '$products' },
+            extensionCount: { $size: '$extensions' }
+        }
+    },
+    {
+        $project: {
+            _id: 1,
+            name: 1,
+            email: 1,
+            avatarURL: 1,
+            role: 1,
+            productCount: 1,
+            extensionCount: 1,
+            disable: { $ifNull: ['$disable', false] },
+            isLogin: { $ifNull: ['$disable', false] }
+        }
+    }
+]);
 
 export {
-    getAllUsers,
+    getAllUsersDetails,
 }
