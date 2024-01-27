113d666acdf198573b1bf38aac25951953bccf52
MaximKraychev <maxim.kraychev@gmail.com>
2024-01-27 19:20:06 +0200

    Add new cases for global error handler

--- server/util/globalErrorHandler.js
+++ server/util/globalErrorHandler.js
@@ -1,5 +1,6 @@
 import { Error as _Error } from 'mongoose';
 
+
 // Global error handler
 export default (err, req, res, next) => {
     // Get status code
@@ -8,15 +9,22 @@ export default (err, req, res, next) => {
     if (err instanceof _Error.ValidationError) {
         // Mongoose validation error
         return res.status(statusCode).json({ message: Object.values(err.errors).map(error => error.message).join(', '), statusCode });
-    } else if (err instanceof _Error) {
+
+    } else if (err.name == 'MongoServerError') {
+
         // Other Mongoose errors
         if (err.code === 11000) {
             // Duplicate key error
-            return res.status(400).json({ message: 'Duplicate key error', statusCode: 400 });
+            return res.status(400).json({ message: 'Duplicate key error', error: err, statusCode: 400 });
+
+        } else if (err.name === 'CastError') {
+            return res.status(400).json({ message: 'Invalid data type', error: err, statusCode: 400 });
+
         } else {
             // Other Mongoose errors
             return res.status(400).json({ message: 'MongoDB error', error: err, statusCode: 400 });
         }
+
     } else if (err.isJoi) {
         // Joi library validation error
         return res.status(statusCode).json({ message: err.details.map(error => error.message).join(', '), statusCode });
