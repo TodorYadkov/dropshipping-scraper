5ef20de04c7ab5676735f022a69e626fc380fa52
Todor Yadkov <toni_12@abv.bg>
2024-01-29 00:06:31 +0200

    Add new sorting functions

--- client/src/util/sortingData.js
+++ client/src/util/sortingData.js
@@ -1,4 +1,4 @@
-import { SORTING_KEYS } from "./constants.js";
+import { SORTING_KEYS, USER_ROLES } from "./constants.js";
 
 export const sortingData = (sortBy, data) => {
     const sortFunctions = {
@@ -23,7 +23,39 @@ export const sortingData = (sortBy, data) => {
         [SORTING_KEYS.EXTENSION_WORKING_DESC]: (a, b) => a.isWork - b.isWork,
         [SORTING_KEYS.EXTENSION_LAST_SEEN_ASC]: (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt),
         [SORTING_KEYS.EXTENSION_LAST_SEEN_DESC]: (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt),
+
+        [SORTING_KEYS.USER_NAME_ASC]: (a, b) => a.name.localeCompare(b.name),
+        [SORTING_KEYS.USER_NAME_DESC]: (a, b) => b.name.localeCompare(a.name),
+        [SORTING_KEYS.USER_EMAIL_ASC]: (a, b) => a.email.localeCompare(b.email),
+        [SORTING_KEYS.USER_EMAIL_DESC]: (a, b) => b.email.localeCompare(a.email),
+        [SORTING_KEYS.USER_LOGIN_STATUS_ASC]: (a, b) => b.isLogin - a.isLogin,
+        [SORTING_KEYS.USER_LOGIN_STATUS_DESC]: (a, b) => a.isLogin - b.isLogin,
+        [SORTING_KEYS.USER_EXTENSIONS_ASC]: (a, b) => a.extensionCount - b.extensionCount,
+        [SORTING_KEYS.USER_EXTENSIONS_DESC]: (a, b) => b.extensionCount - a.extensionCount,
+        [SORTING_KEYS.USER_PRODUCTS_ASC]: (a, b) => a.productCount - b.productCount,
+        [SORTING_KEYS.USER_PRODUCTS_DESC]: (a, b) => b.productCount - a.productCount,
+        [SORTING_KEYS.USER_ROLE_USER]: customRoleSort(USER_ROLES.USER),
+        [SORTING_KEYS.USER_ROLE_PREMIUM]: customRoleSort(USER_ROLES.PREMIUM),
+        [SORTING_KEYS.USER_ROLE_ADMIN]: customRoleSort(USER_ROLES.ADMIN),
+        [SORTING_KEYS.USER_ACCOUNT_STATUS_ASC]: (a, b) => b.isLogin - a.isLogin,
+        [SORTING_KEYS.USER_ACCOUNT_STATUS_DESC]: (a, b) => a.isLogin - b.isLogin,
     };
 
     data.sort(sortFunctions[sortBy] || ((a, b) => a - b));
+}
+
+function customRoleSort(selectedRole) {
+    return (a, b) => {
+
+        if (a.role === selectedRole && b.role === selectedRole) {
+            return 0;
+        } else if (a.role === selectedRole) {
+            return -1;
+        } else if (b.role === selectedRole) {
+            return 1;
+        } else {
+            // Sort non-selected roles alphabetically
+            return a.role.localeCompare(b.role);
+        }
+    };
 }
