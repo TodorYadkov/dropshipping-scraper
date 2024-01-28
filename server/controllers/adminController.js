6bbccaa75f0194f6487b3b21d22750cf31f0cce7
Todor Yadkov <toni_12@abv.bg>
2024-01-29 00:06:53 +0200

    Add new endpoints

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,24 +1,84 @@
 import { Router } from 'express';
 
-import { getAllUsersDetails } from '../services/adminService.js';
-import { getUserById } from '../services/userService.js';
 import { isUserLogged, isUserRole } from '../middlewares/guards.js';
 import { preload } from '../middlewares/preloader.js';
-
-import { USER_ROLES } from '../environments/userRoles.js';
 import { PRELOAD_OPTIONS } from '../environments/preloadOptions.js';
+import { USER_ROLES } from '../environments/userRoles.js';
+import { getUserById } from '../services/userService.js';
+import {
+    getAllUsersDetails,
+    adminUserStatistics,
+    updateUserRole,
+    updateUserStatusToBeDisabled,
+    updateUserStatusToBeEnabled
+} from '../services/adminService.js';
 
 const adminController = Router();
 
 // Get all users
 adminController.get('/', isUserLogged, preload(getUserById, PRELOAD_OPTIONS.ROLE), isUserRole(USER_ROLES.ADMIN), async (req, res, next) => {
     try {
-        const allUsers = await getAllUsersDetails();
+        const userId = req.user._id;
+
+        const allUsers = await getAllUsersDetails(userId);
 
         res.status(200).json(allUsers);
-    } catch (err) {
-        next(err);
+    } catch (error) {
+        next(error);
     }
-})
+});
+
+// Update user role from admin
+adminController.put('/role', isUserLogged, preload(getUserById, PRELOAD_OPTIONS.ROLE), isUserRole(USER_ROLES.ADMIN), async (req, res, next) => {
+    try {
+        const userId = req.body._id;
+        const role = req.body.role;
+
+        const updatedUser = await updateUserRole(userId, role);
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Update user status to be disable (banned)
+adminController.put('/disable', isUserLogged, preload(getUserById, PRELOAD_OPTIONS.ROLE), isUserRole(USER_ROLES.ADMIN), async (req, res, next) => {
+    try {
+        const userId = req.body._id;
+
+        const disabledUser = await updateUserStatusToBeDisabled(userId);
+
+        res.status(200).json(disabledUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Update user status to be enabled (working)
+adminController.put('/enable', isUserLogged, preload(getUserById, PRELOAD_OPTIONS.ROLE), isUserRole(USER_ROLES.ADMIN), async (req, res, next) => {
+    try {
+        const userId = req.body._id;
+
+        const enabledUser = await updateUserStatusToBeEnabled(userId);
+
+        res.status(200).json(enabledUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Get statistics data only for admin panel
+adminController.get('/statistic', isUserLogged, preload(getUserById, PRELOAD_OPTIONS.ROLE), isUserRole(USER_ROLES.ADMIN), async (req, res, next) => {
+    try {
+        const userId = req.user._id;
+
+        const adminStatistics = await adminUserStatistics(userId);
+
+        res.status(200).json(adminStatistics);
+    } catch (error) {
+        next(error);
+    }
+});
 
 export { adminController };
