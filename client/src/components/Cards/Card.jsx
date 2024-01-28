5756de89d80ac4c3a01279c2176847273a675f11
Todor Yadkov <toni_12@abv.bg>
2024-01-29 00:02:00 +0200

    Add CardUsers to card component

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -1,8 +1,10 @@
 import { useMemo } from 'react';
 
 import { DATA_TYPES } from '../../util/constants.js';
+
 import { CardProducts } from './CardProducts.jsx';
 import { CardExtension } from './CardExtension.jsx';
+import { CardUsers } from './CardUsers.jsx';
 
 export const Card = ({ dataTypes, data, onModalClick }) => {
 
@@ -13,10 +15,13 @@ export const Card = ({ dataTypes, data, onModalClick }) => {
             case DATA_TYPES.PRODUCT:
                 CardComponent = <CardProducts products={data} onModalClick={onModalClick} />
                 break;
+
             case DATA_TYPES.EXTENSION:
                 CardComponent = <CardExtension extensions={data} onModalClick={onModalClick} />
                 break;
+
             case DATA_TYPES.USER:
+                CardComponent = <CardUsers usersData={data} onModalClick={onModalClick} />
                 break;
         }
 
