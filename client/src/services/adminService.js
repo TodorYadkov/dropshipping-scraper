27809aaee7b82ff303616b5f7dc40d94223b0a63
Todor Yadkov <toni_12@abv.bg>
2024-01-29 00:05:32 +0200

    Add new function

--- client/src/services/adminService.js
+++ client/src/services/adminService.js
@@ -1,9 +1,21 @@
 import { SERVER_PATHS } from '../util/paths.js';
 
 export const adminService = (api) => {
-    const getAllUsers = async () => api.get(SERVER_PATHS.GET_ALL_USERS);
+    const getAllUsers = async () => api.get(SERVER_PATHS.ADMIN_GET_USERS);
+
+    const getAdminStatistic = async () => api.get(SERVER_PATHS.ADMIN_USERS_STATISTIC);
+
+    const updateUserRole = async (data) => api.put(SERVER_PATHS.ADMIN_UPDATE_USER_ROLE, data);
+
+    const updateUserStatusToBeDisabled = async (data) => api.put(SERVER_PATHS.ADMIN_DISABLE_USER_STATUS, data);
+
+    const updateUserStatusToBeEnabled = async (data) => api.put(SERVER_PATHS.ADMIN_ENABLE_USER_STATUS, data);
 
     return {
-        getAllUsers
+        getAllUsers,
+        getAdminStatistic,
+        updateUserRole,
+        updateUserStatusToBeDisabled,
+        updateUserStatusToBeEnabled,
     };
 };
