aa417e1f56b5594201cc87a050e84aff7a33a745
Todor Yadkov <toni_12@abv.bg>
2024-01-29 00:01:03 +0200

    Add functionality to change user role

--- client/src/components/DropdownRoleSelector.jsx
+++ client/src/components/DropdownRoleSelector.jsx
@@ -1,38 +1,60 @@
-/* eslint-disable no-unused-vars */
 import { useContext, useState } from 'react';
 
-import { AdminPanelContext } from '../contexts/AdminPanelContext.jsx';
 import { USER_ROLES } from '../util/constants.js';
 
-export const DropdownRoleSelector = ({ role, userId }) => {
-    const [value, setValue] = useState(role);
-    const { updateUserData } = useContext(AdminPanelContext);
+import { AdminPanelContext } from '../contexts/AdminPanelContext.jsx';
 
-    function handleSelectChange(e) {
+import { AlertError } from './Alerts/AlertError.jsx';
 
-        updateUserData({ _id: userId, role: 'admin' })
-        setValue(e.target.value);
-    }
+export const DropdownRoleSelector = ({ role, userDetails }) => {
+    const [serverErrorMessage, setServerErrorMessage] = useState('');
+
+    const { updateUserData, updateUserRole } = useContext(AdminPanelContext);
+
+    const handleSelectChangeHandler = async (e) => {
+        try {
+            const dataForServer = { _id: userDetails._id, role: e.target.value };
+
+            const updatedUser = await updateUserRole(dataForServer);
+
+            updateUserData({ ...userDetails, ...updatedUser });
+        } catch (error) {
+            setServerErrorMessage(error.message);
+            console.error(error);
+        }
+    };
+
+    const removeServerErrorMessageHandler = () => {
+        setServerErrorMessage('');
+    };
 
     return (
-        <div className="relative">
-            <select
-                className="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white appearance-none border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
-                value={value}
-                onChange={handleSelectChange}
-            >
-                {Object.values(USER_ROLES).map((name, index) => (<option key={index} value={name}>{name}</option>))}
-
-            </select>
-            {console.log(role)}
-            <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
-                <svg
-                    className="w-4 h-4 fill-current"
-                    viewBox="0 0 20 20"
+        <>
+            <div className="relative">
+                <select
+                    className="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white appearance-none border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+                    value={role}
+                    onChange={handleSelectChangeHandler}
                 >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+                    {Object.values(USER_ROLES).map((name, index) => (<option key={index} value={name}>{name}</option>))}
+
+                </select>
+
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                    <svg
+                        className="w-4 h-4 fill-current"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                </div>
             </div>
-        </div>
+
+            {serverErrorMessage && ((
+                <div className="absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <AlertError message={serverErrorMessage} close={removeServerErrorMessageHandler} />
+                </div>
+            ))}
+        </>
     );
 };
