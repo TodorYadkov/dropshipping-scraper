c4ba6fde1fdc665899829502c267e7b8b81810e4
Todor Yadkov <toni_12@abv.bg>
2024-01-30 17:09:43 +0200

    Fix heading to bold to be same like a other table headings

--- client/src/components/Tables/TableHeader.jsx
+++ client/src/components/Tables/TableHeader.jsx
@@ -3,7 +3,7 @@ export const TableHeader = ({ headings }) => {
         <thead>
             <tr>
                 {headings.map((heading, index) => (
-                    <th key={index} className="px-5 py-3 text-xs font-semibold tracking-wider text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200 whitespace-nowrap cursor-default">
+                    <th key={index} className="px-5 py-3 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200 whitespace-nowrap cursor-default">
                         {heading}
                     </th>
                 ))}
